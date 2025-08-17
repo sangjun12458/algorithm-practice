8e2c0947274b3e45ef55784d3fb784b4afb0339b
sangjun12458 <sjpark514@khu.ac.kr>
2025-08-17 18:11:42 +0900

    feat: 백준 16236 아기 상어 문제 파이썬 풀이 및 개선점 추가

--- problems/baekjoon/16236/python/main.py
+++ problems/baekjoon/16236/python/main.py
@@ -4,23 +4,53 @@ from collections import deque
 input = sys.stdin.readline
 n = int(input())
 space = [list(map(int, input().split())) for _ in range(n)]
-fishes = []
-shark = 2
+shark = [-1, -1, -1, -1]
 total_time = 0
+delta = [[-1, 0], [0, -1], [0, 1], [1, 0]]
 
 for i in range(n):
     for j in range(n):
-        if space[i][j] in range(1, 7):
-            fishes.append((i, j, space[i][j]))
+        if space[i][j] == 9:
+            shark = [i, j, 2, 0]
 
 while True:
-    feed = -1
+    feed = [-1, -1, 999]
+    bfs = deque()
+    visited = [[False] * n for _ in range(n)]
+    bfs.append([shark[0], shark[1], 0])
+    while bfs:
+        now = bfs.popleft()
+        if visited[now[0]][now[1]]:
+            continue
+        visited[now[0]][now[1]] = True
+        if space[now[0]][now[1]] > 0 and space[now[0]][now[1]] < shark[2]:
+            if now[2] < feed[2]:
+                feed = now
+            elif now[2] == feed[2]:
+                if now[0] < feed[0]:
+                    feed = now
+                elif now[0] == feed[0]:
+                    if now[1] < feed[1]:
+                        feed = now
+        for di, dj in delta:
+            ni, nj = now[0] + di, now[1] + dj
+            if ni < 0 or nj < 0 or ni >= n or nj >= n: 
+                continue
+            if visited[ni][nj]:
+                continue
+            if space[ni][nj] in range(0, shark[2]+1):
+                bfs.append([ni, nj, now[2] + 1])
 
-    
-        
-        
-
-
-
+    if feed[0] == -1:
+        break
+    else:
+        space[feed[0]][feed[1]] = 9
+        space[shark[0]][shark[1]] = 0
+        shark[0], shark[1] = feed[0], feed[1]
+        shark[3] += 1
+        if shark[2] == shark[3]:
+            shark[2] += 1
+            shark[3] = 0
+        total_time += feed[2]
 
 print(total_time)
---
+++ problems/baekjoon/16236/python/main_optimized.py
@@ -0,0 +1,52 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+n = int(input())
+space = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if space[i][j] == 9:
+            sx, sy = i, j
+            space[i][j] = 0
+
+size, eaten, total_time = 2, 0, 0
+dirs = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+
+def bfs(x, y, size):
+    q = deque([(x, y, 0)])
+    visited = [[0]*n for _ in range(n)]
+    visited[x][y] = 1
+    fish = []
+
+    while q:
+        i, j, d = q.popleft()
+        for di, dj in dirs:
+            ni, nj = i + di, j + dj
+            if 0 <= ni < n and 0 <= nj < n and not visited[ni][nj]:
+                if space[ni][nj] <= size:
+                    visited[ni][nj] = 1
+                    if 0 < space[ni][nj] < size:
+                        fish.append((d+1, ni, nj))
+                    else:
+                        q.append((ni, nj, d+1))
+    if not fish:
+        return None
+    else:
+        return min(fish)
+
+while True:
+    res = bfs(sx, sy, size)
+    if res is None:
+        break
+    dist, fx, fy = res
+    total_time += dist
+    sx, sy = fx, fy
+    space[fx][fy] = 0
+    eaten += 1
+    if size == eaten:
+        size += 1
+        eaten = 0
+
+print(total_time)
