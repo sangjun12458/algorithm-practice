c44a36c33fdc95ed8db07c6cef451bda53d9ad9c
sangjun12458 <sjpark514@khu.ac.kr>
2025-08-27 02:23:40 +0900

    boj 7569 토마토 python solution 개선

--- problems/baekjoon/1-10k/7569/main.py
+++ problems/baekjoon/1-10k/7569/main.py
@@ -1,48 +1,35 @@
 # 7569. 토마토
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 m, n, h = map(int, input().split())
 tomatoes = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
 dirs = [(-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1)]
 
-unripens = []
+q = deque()
+
 for i in range(h):
     for j in range(n):
         for k in range(m):
-            if tomatoes[i][j][k] == 0:
-                unripens.append((i, j, k))
+            if tomatoes[i][j][k] == 1:
+                q.append((i, j, k))
 
-result = 0
-while True:
-    ripens = []
-    print(unripens)
-    for i, j, k in unripens:
-        is_ripen = False
-        print(i, j, k)
-        for di, dj, dk in dirs:
-            if is_ripen: continue
-            ni, nj, nk = i + di, j + dj, k + dk
-            if not (0 <= ni < h): continue
-            if not (0 <= nj < n): continue
-            if not (0 <= nk < m): continue
-            if tomatoes[ni][nj][nk] == 1:
-                unripens.remove((i, j, k))
-                ripens.append((i, j, k))
-                is_ripen = True
+while q:
+    i, j, k = q.popleft()
+    for di, dj, dk in dirs:
+        ni, nj, nk = i + di, j + dj, k + dk
+        if 0 <= ni < h and 0 <= nj < n and 0 <= nk < m:
+            if tomatoes[ni][nj][nk] == 0:
+                tomatoes[ni][nj][nk] = tomatoes[i][j][k] + 1
+                q.append((ni, nj, nk))
 
-    if len(ripens) == 0:
-        if len(unripens) == 0:
-            print(result)
-        else:
+max_day = 0
+for box in tomatoes:
+    for row in box:
+        if 0 in row:
             print(-1)
-        break
+            sys.exit(0)
+        max_day = max(max_day, max(row))
 
-    result += 1
-    for i, j, k in ripens:
-        tomatoes[i][j][k] = 1
-
-    print()
-    for rc in tomatoes:
-        for row in rc:
-            print(row)
+print(max_day - 1)
