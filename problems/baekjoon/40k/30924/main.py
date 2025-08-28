dc5cfea65ddcf1563260cf90b40e8af78eb02148
sangjun12458 <sjpark514@khu.ac.kr>
2025-08-28 20:38:09 +0900

    boj 30924 A+B - 10 (제2편) python solution 추가

---
+++ problems/baekjoon/40k/30924/main.py
@@ -0,0 +1,28 @@
+# 30924. A+B - 10 (제2편)
+import random
+
+check = [0] * 10001
+a, b = 0, 0
+
+for _ in range(19997):
+    if a == 0:
+        n = random.randint(1, 10000)
+        while check[n] == 1:
+            n = random.randint(1, 10000)
+        check[n] = 1
+        print("? A", n)
+        resp = int(input().strip())
+        if resp == 1:
+            a = n
+    else:
+        n = random.randint(1, 10000)
+        while check[n] == 2:
+            n = random.randint(1, 10000)
+        check[n] = 2
+        print("? B", n)
+        resp = int(input().strip())
+        if resp == 1:
+            b = n
+    if b != 0:
+        print("!", a+b)
+        break
