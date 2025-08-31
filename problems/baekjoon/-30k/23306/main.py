b10a887c84d8a8b91189b1c6c045228fa2d75ae4
sangjun12458 <sjpark514@khu.ac.kr>
2025-08-31 23:01:33 +0900

    boj 11659 구간 합 구하기 4 python solution 추가

---
+++ problems/baekjoon/-30k/23306/main.py
@@ -0,0 +1,12 @@
+# 23306. binary는 호남선
+
+n = int(input().strip())
+
+while True:
+    k = range(1, n+1)
+    print("?", k, flush=True)
+    resp = input().strip()
+    if resp == '0':
+        pass
+    elif resp == '1':
+        pass 
