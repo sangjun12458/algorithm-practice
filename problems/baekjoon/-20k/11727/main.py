a489c61f18f96431a56f81de64df421bc74dc4e5
sangjun12458 <sjpark514@khu.ac.kr>
2025-09-22 00:12:25 +0900

    Modify boj 11727 python solution runtime error:

--- problems/baekjoon/-20k/11727/main.py
+++ problems/baekjoon/-20k/11727/main.py
@@ -4,8 +4,9 @@ CONST = 10007
 n = int(input().strip())
 tiling = [0] * (n + 1)
 tiling[1] = 1
-tiling[2] = 3
-for i in range(3, n + 1):
-    tiling[i] = (tiling[i - 1] + tiling[i - 2] * 2) % CONST
+if n > 1:
+    tiling[2] = 3
+    for i in range(3, n + 1):
+        tiling[i] = (tiling[i - 1] + tiling[i - 2] * 2) % CONST
 
 print(tiling[n])
