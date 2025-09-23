5a1d0edb1e3b2f7baa877ce77b69eb2d9eec2ffd
sangjun12458 <sjpark514@khu.ac.kr>
2025-09-23 22:16:51 +0900

    Add boj 2193 python solution

---
+++ problems/baekjoon/-10k/2193/main.py
@@ -0,0 +1,8 @@
+# 2193. 이친수
+n = int(input().strip())
+dp = [[0, 0] for _ in range(n+1)]
+dp[1][1] = 1
+for i in range(2, n+1):
+    dp[i][0] = dp[i-1][0] + dp[i-1][1]
+    dp[i][1] = dp[i-1][0]
+print(sum(dp[n]))
