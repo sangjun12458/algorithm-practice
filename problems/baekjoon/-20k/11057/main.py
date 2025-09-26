1224be9774ec24d8078e181e304dd3adef018220
sangjun12458 <sjpark514@khu.ac.kr>
2025-09-27 01:46:15 +0900

    Modify boj 11057 python solution

--- problems/baekjoon/-20k/11057/main.py
+++ problems/baekjoon/-20k/11057/main.py
@@ -1,17 +1,14 @@
 # 11057. 오르막 수
-import sys
-input = sys.stdin.readline
 
 n = int(input().strip())
 MOD = 10007
 
-dp = [[0] * 10 for _ in range(n + 1)]
-
-for d in range(10):
-    dp[1][d] = 1
-
-for i in range(2, n + 1):
-    for d in range(10):
-        dp[i][d] = sum(dp[i-1][:d+1]) % MOD
-
-print(sum(dp[n]) % MOD)
+dp = [1] * 10
+for _ in range(1, n):
+    total = sum(dp) % MOD
+    for i in range(10):
+        tmp = dp[i]
+        dp[i] = total
+        total -= tmp
+        
+print(sum(dp) % MOD)
