23e185f7d672dd3722c5c49a7a25e6d2fedf2456
sangjun12458 <sjpark514@khu.ac.kr>
2025-10-05 23:46:17 +0900

    Add boj 2133 python solution

--- problems/baekjoon/-10k/2133/main.py
+++ problems/baekjoon/-10k/2133/main.py
@@ -1,8 +1,9 @@
 # 2133. 타일 채우기
 n = int(input().strip())
 dp = [0] * (n+1)
-if n >= 2:
-    dp[2] = 3
-for i in range(4, n+1):
-    dp[i] = dp[i-2] * 3 + dp[i-1]
+dp[0] = 1
+cum = 1
+for i in range(2, n+1, 2):
+    dp[i] = dp[i-2] + cum * 2
+    cum += dp[i]
 print(dp[n])
