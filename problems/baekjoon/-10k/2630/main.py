49637e6037ccc7a400496b0e44e0dab6cda58083
sangjun12458 <sjpark514@khu.ac.kr>
2025-10-09 22:33:02 +0900

    Add boj 2630 python solution

--- problems/baekjoon/-10k/2630/main.py
+++ problems/baekjoon/-10k/2630/main.py
@@ -1,6 +1,33 @@
 # 2630. 색종이 만들기
 import sys
+from collections import deque
 input = sys.stdin.readline
+q = deque()
+
 n = int(input().strip())
 paper = [input().split() for _ in range(n)]
-print(paper)
+
+q.append((0, 0, n))
+whites, blues = 0, 0
+while q:
+    x, y, l = q.popleft()
+    color = paper[x][y]
+    isSingleColor = True
+    for i in range(x, x+l):
+        for j in range(y, y+l):
+            if paper[i][j] != color:
+                isSingleColor = False
+    if isSingleColor:
+        if color == '0':
+            whites += 1
+        else:
+            blues += 1
+    else:
+        if l >= 2:
+            nl = l // 2
+            q.append((x, y, nl))
+            q.append((x, y + nl, nl))
+            q.append((x + nl, y, nl))
+            q.append((x + nl, y + nl, nl))
+
+print(whites, blues, sep='\n')
