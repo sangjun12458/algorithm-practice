1b5ecc7fb71d4f1884f29b21063182afdde1e619
sangjun12458 <sjpark514@khu.ac.kr>
2025-10-12 23:31:12 +0900

    Modify boj 15654 python solution

--- problems/baekjoon/-20k/15654/main.py
+++ problems/baekjoon/-20k/15654/main.py
@@ -1,2 +1,23 @@
 # 15654. N과 M (5)
+import sys
+sys.setrecursionlimit(10**6)
+
 n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+visited = [0] * (n + 1)
+nums.sort()
+
+def dfs(s, depth):
+    if depth == m:
+        print(s.strip())
+        return
+
+    for i in range(n):
+        num = nums[i]
+        if visited[i] == 1:
+            continue
+        visited[i] = 1
+        dfs(s + f'{num} ', depth + 1)
+        visited[i] = 0
+
+dfs('', 0)
