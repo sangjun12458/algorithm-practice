7b16b484004f46313300f9127c32b86defc73fcc
sangjun12458 <sjpark514@khu.ac.kr>
2025-10-13 22:23:31 +0900

    Add boj 15663 python solution

---
+++ problems/baekjoon/-20k/15663/main.py
@@ -0,0 +1,8 @@
+# 15663. N과 M (9)
+from itertools import permutations
+
+n, m = map(int, input().split())
+nums = map(int, input().split())
+
+for p in sorted(set(permutations(nums, m))):
+    print(*p)
