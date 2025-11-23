ee98a2c83a5e4a55c4fc0507b08f06d5b4b7e4ae
sangjun12458 <sjpark514@khu.ac.kr>
2025-11-23 23:46:17 +0900

    feat(boj): partial solution for 2448 - some test cases still failing

--- problems/baekjoon/-10k/2448/main.py
+++ problems/baekjoon/-10k/2448/main.py
@@ -1,18 +1,38 @@
 # 별 찍기 - 11
 import math
+tri = ["  *  ", " * * ", "*****"]
+
+def print_tri(cnt, pos):
+    for t in tri:
+        s = ""
+        for p in pos:
+            s += " "*(3*(p-1) - len(s)) + t
+        print(s)
 
 n = int(input())
-
-def triangle(n, k, r):
-    if k == 0:
-        print(f'{"*":^5}')
-        print(f'{"* *":^5}')
-        print('*****')
-    else:
-        #nk = k // 2
-        triangle(n, k - 1, r)
-        triangle(n, k - 1, r - (k - 1) * 6)
-        triangle(n, k - 1, r + (k - 1) * 6)
-
 k = int(math.log2(n // 3))
-triangle(n, k, k * 6)
+pattern = []
+pattern.append((1, [2**k]))
+for _ in range(k):
+    next_pattern = []
+    for cnt, pos in pattern:
+        prev_cnt, prev_pos = next_pattern[-1] if next_pattern else pattern[-1]
+        next_cnt, next_pos = cnt * 2, []
+        if next_cnt == prev_cnt:
+            for idx in range(len(prev_pos)):
+                next_pos.append(prev_pos[idx] + idx % 2 * 2 - 1)
+        elif next_cnt > prev_cnt:
+            for p in prev_pos:
+                next_pos.append(p - 1)
+                next_pos.append(p + 1)
+        else:
+            for p in pos:
+                next_pos.append()
+                next_pos.append()
+        next_pattern.append((next_cnt, next_pos))
+    pattern.extend(next_pattern)
+
+print(pattern)
+
+for cnt, pos in pattern:
+    print_tri(cnt, pos)
---
+++ problems/baekjoon/-20k/23291/main.py
@@ -0,0 +1,37 @@
+# 23291. 어항 정리
+
+N, K = map(int, input().split())
+fishes = list(map(int, input().split()))
+ans = 0
+
+def check():
+    return
+
+def add():
+    return
+
+def process_1():
+    return
+
+def control():
+    return
+
+def serialize():
+    return
+
+def process_2():
+    return
+
+while True:
+    if check():
+        print(ans)
+        break
+
+    add()
+    process_1()
+    control()
+    serialize()
+    process_2()
+    control()
+    serialize()
+    ans += 1
