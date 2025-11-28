e72b22df1c688fdc304f4f15cca1bd9542493916
sangjun12458 <sjpark514@khu.ac.kr>
2025-11-28 22:57:50 +0900

    fix(boj) finalize solution for problem 12851 - now passes all test cases

--- problems/baekjoon/-20k/12851/main.py
+++ problems/baekjoon/-20k/12851/main.py
@@ -2,30 +2,35 @@
 from collections import deque
 MAX_NUM = 10**5
 
+def step(y):
+    global answer
+    if 0 <= y <= MAX_NUM and time[x] + 1 <= time[y]:
+        if y == N:
+            time[N] = time[x] + 1
+            if time[x] + 1 < time[N]:
+                answer = 1
+            else:
+                answer += 1
+        else:
+            if time[x] + 1 < time[N]:
+                time[y] = time[x] + 1
+                q.append(y)
+
 N, K = map(int, input().split())
-time = [-1] * (MAX_NUM + 1)
+time = [MAX_NUM] * (MAX_NUM + 1)
 time[K] = 0
 q = deque([K])
 
 answer = 0
-while q and time[N] == -1:
+while q:
     x = q.popleft()
-    # if x == N:
-    #     if time[N] != -1:
-            
-            
-    # else:
-
-    for k in [-1, 1]:
-        y = x + k
-        if 0 <= y <= MAX_NUM and time[y] == -1:
-            time[y] = time[x] + 1
-            q.append(y)
+    step(x - 1)
+    step(x + 1)
     if x % 2 == 0:
-        y = x // 2
-        if 0 <= y <= MAX_NUM and time[y] == -1:
-            time[y] = time[x] + 1
-            q.append(y)
-
+        step(x // 2)
 
 print(time[N])
+if N == K:
+    print(1)
+else:
+    print(answer)
