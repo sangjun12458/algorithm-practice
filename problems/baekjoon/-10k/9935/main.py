e07e543e9f0353c96691d720d2ff1e4ef6d98e73
sangjun12458 <sjpark514@khu.ac.kr>
2025-11-29 19:23:35 +0900

    feat(boj): partial solution for 9935 - some test cases sill failing

--- problems/baekjoon/-10k/9935/main.py
+++ problems/baekjoon/-10k/9935/main.py
@@ -3,6 +3,14 @@
 a = input().strip()
 b = input().strip()
 
-ai = 0
-bi = 0
+ap = 0
+bp = 0
 
+while ap < len(a):
+    if a[ap:ap+len(b)] == b:
+        a = a[:ap] + a[ap+len(b):]
+        ap = 0
+    else:
+        ap += 1
+    
+print(a)
