ee82e510dd668a2ab26f753a22c8ba66d3e35ad4
sangjun12458 <sjpark514@khu.ac.kr>
2025-12-07 23:57:45 +0900

    Implement optimized stack solution for BOJ 9935

--- problems/baekjoon/-10k/9935/main.py
+++ problems/baekjoon/-10k/9935/main.py
@@ -1,51 +1,15 @@
 # 9935. 문자열 폭발
 
-# a = input().strip()
-# b = input().strip()
-N = 1_000_000
-a = 'a' * (N//2) + 'b' * (N//2)
-b = 'ab'
+a = input().strip()
+b = input().strip()
+l = len(b)
 
 stack = []
-pa = pb = 0
 
+for c in a:
+    stack.append(c)
+    if len(stack) >= l and "".join(stack[-l:]) == b:
+        del stack[-l:]
 
-while pa < len(a):
-    #print(pa, pb, stack)
-    print(len(stack))
-    if a[pa] == b[pb]:
-        stack.append((b[pb], pb))
-        pa += 1
-        pb += 1
-        if pb == len(b):
-            a = a[:pa-pb] + a[pa:]
-            pa -= pb
-            for _ in range(len(b)):
-                stack.pop()
-            if stack:
-                _, pb = stack[-1]
-                pb += 1
-            else:
-                pb = 0
-    else:
-        pb = 0
-        if a[pa] != b[pb]:
-            pa += 1
-
-# while ap < len(a):
-#     if a[ap:ap+len(b)] == b:
-#         a = a[:ap] + a[ap+len(b):]
-#         ap = 0
-#     else:
-#         ap += 1
-
-if a:
-    print(a)
-else:
-    print('FRULA')
-
-# mirkovC4nizCC44
-# C4
-
-# 12ab112ab2ab
-# 12ab
+result = "".join(stack)
+print(result if result else "FRULA")
