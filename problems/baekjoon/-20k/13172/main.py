725ae4003aa1d5a1317a75bd8ff781de7e34419b
sangjun12458 <sjpark514@khu.ac.kr>
2025-12-10 14:05:26 +0900

    Add optimized solution for BOJ 13172

--- problems/baekjoon/-20k/13172/main.py
+++ problems/baekjoon/-20k/13172/main.py
@@ -3,12 +3,15 @@ import sys
 input = sys.stdin.readline
 MOD = 1_000_000_007
 
+def modinv(a):
+    # a^(MOD-2) % MOD  (Fermat’s little theorem)
+    return pow(a, MOD - 2, MOD)
+
 M = int(input())
-
-def modulo_inverse():
-    return
-
 ans = 0
+
 for _ in range(M):
     N, S = map(int, input().split())
-    ans += modulo_inverse(N, S)
+    ans = (ans + S * modinv(N)) % MOD
+
+print(ans)
