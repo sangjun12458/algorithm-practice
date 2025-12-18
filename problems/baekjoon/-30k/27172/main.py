bb2bb1bf8adbc8071a0f68cf76d8c56328566f70
sangjun12458 <sjpark514@khu.ac.kr>
2025-12-18 15:16:32 +0900

    fix(boj): finalize solution for problem 27172 - now passes all test cases

--- problems/baekjoon/-30k/27172/main.py
+++ problems/baekjoon/-30k/27172/main.py
@@ -3,28 +3,20 @@ import math
 MAX_NUM = 1_000_000
 
 N = int(input())
-players = list(map(int, input().split()))
-cards = [-1] * (MAX_NUM + 1)
+card_of_player = list(map(int, input().split()))
+player_of_card = [-1] * (MAX_NUM + 1)
 scores = [0] * N
 
+max_value = 0
 for i in range(N):
-    cards[players[i]] = i
+    player_of_card[card_of_player[i]] = i
+    max_value = max(max_value, card_of_player[i])
 
-for i in range(1, 1001):
-    for j in range(i*i, MAX_NUM+1):
-        #print(i, j)
-
-        if j % i != 0 or cards[j] == -1:
-            continue
-        if cards[i] != -1:
-            scores[cards[i]] += 1
-            scores[cards[j]] -= 1
-
-        if i * i == j:
-            continue
-
-        if cards[j // i] != -1:
-            scores[cards[j // i]] += 1
-            scores[cards[j]] -= 1
+for num in card_of_player:
+    for k in range(num*2, max_value+1, num):
+        if player_of_card[k] == -1: continue
+        if k % num != 0: continue
+        scores[player_of_card[num]] +=1 
+        scores[player_of_card[k]] -= 1
 
 print(*scores)
