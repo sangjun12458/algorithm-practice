4fc1454176d5a4965aab08b5324d95b2d279a87a
sangjun12458 <sjpark514@khu.ac.kr>
2025-12-21 19:46:06 +0900

    feat(boj): Add complete solution for BOJ 2143

--- problems/baekjoon/-10k/2143/main.py
+++ problems/baekjoon/-10k/2143/main.py
@@ -18,3 +18,25 @@ for i in range(m):
 dict_A = dict()
 dict_B = dict()
 
+for i in range(n):
+    for j in range(i+1, n+1):
+        prefix_sum = sum_A[j] - sum_A[i]
+        if dict_A.get(prefix_sum):
+            dict_A[prefix_sum] += 1
+        else:
+            dict_A[prefix_sum] = 1
+
+for i in range(m):
+    for j in range(i+1, m+1):
+        prefix_sum = sum_B[j] - sum_B[i]
+
+        if dict_B.get(prefix_sum):
+            dict_B[prefix_sum] += 1
+        else:
+            dict_B[prefix_sum] = 1
+
+ans = 0
+for (key, value) in dict_A.items():
+    if dict_B.get(T-key):
+        ans += value * dict_B[T-key]
+print(ans)
