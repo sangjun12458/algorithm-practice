7ecc139eb5822125e1bc1e5400e9876d459921d1
sangjun12458 <sjpark514@khu.ac.kr>
2025-12-27 13:11:54 +0900

    Add solution with TLE on large inputs

--- problems/baekjoon/-10k/2473/main.py
+++ problems/baekjoon/-10k/2473/main.py
@@ -1,21 +1,59 @@
 # 2473. 세 용액
+INF = 10**9
 
 N = int(input())
 solution = list(map(int, input().split()))
+
+# N = 5000
+# neg = [-10**9 + i for i in range(2498)]
+# mid = [-2, -1, 3]
+# pos = [10**9 - 2499 + i for i in range(2499)]
+# solution = neg + mid + pos
 solution.sort()
 
-print(solution)
+best = INF * 3
+ans = (-1, -1, -1)
 
-ab = []
-
-start = 0
-end = N-1
-
-while start < end:
-    ab.append(solution[start] + solution[end])
-    if abs(solution[start]) < abs(solution[end]):
-        end -= 1
+while len(solution) >= 3:
+    s1 = solution.pop()
+    if s1 < 0:
+        total = s1 + solution[-1] + solution[-2]
+        if abs(total) < abs(best):
+            best = total
+            ans = (solution[-2], solution[-1], s1)
+            break
+    base = -s1//2
+ 
+    #s2, s3 = -INF, INF
+    left, right = -1, len(solution)
+    #print(s1, left, right)
+    for idx, s in enumerate(solution):
+        if s > base:
+            right = idx
+            break
+        left = idx
+    
+    if left == -1:
+        total = s1 + solution[0] + solution[1]
+        if abs(total) < abs(best):
+            best = total
+            ans = (solution[0], solution[1], s1)
+    elif right == len(solution):
+        total = s1 + solution[-1] + solution[-2]
+        if abs(total) < abs(best):
+            best = total
+            ans = (solution[-2], solution[-1], s1)
     else:
-        start += 1
+        while left >= 0 and right < len(solution):
+            s2 = solution[left]
+            s3 = solution[right]
+            total = s1 + s2 + s3
+            if abs(total) < abs(best):
+                best = total
+                ans = (s2, s3, s1)
+            if abs(s2 - base) < abs(s3 - base):
+                left -= 1
+            else:
+                right += 1
 
-print(ab)
+print(*ans)
