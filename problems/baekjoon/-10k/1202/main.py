a2a92b0f298a250dff90dd3b48d645c1d9585004
sangjun12458 <sjpark514@khu.ac.kr>
2026-01-06 22:25:27 +0900

    fix(boj): incomplete solution for 1202 - some test cases failed with TLE

--- problems/baekjoon/-10k/1202/main.py
+++ problems/baekjoon/-10k/1202/main.py
@@ -5,27 +5,20 @@ input = sys.stdin.readline
 N, K = map(int, input().split())
 jewel = [tuple(map(int, input().split())) for _ in range(N)]
 bag = [int(input()) for _ in range(K)]
+visited = [0] * K
 
-jewel.sort(key = lambda x: (-x[1], x[0]))
-bag.sort(reverse=True)
+jewel.sort(key = lambda x: -x[1])
+bag.sort()
 
 answer = []
-a, b = 0, 0
-while a < N and b < K:
-    weight = jewel[a][0]
-    value = jewel[a][1]
-    limit = bag[b]
+for i in range(N):
+    weight = jewel[i][0]
+    value = jewel[i][1]
 
-    a += 1
-    b += 1
+    for j in range(K):
+        if not visited[j] and weight <= bag[j]:
+            visited[j] = True
+            answer.append(value)
+            break
 
-for i in range(K):
-    weight = bag[i]
-    start = 0
-    while start < N and jewel[start][0] > weight:
-        start += 1
-    answer.append(jewel[start][1])
-
-print(jewel)
-print(bag)
-print(answer)
+print(sum(answer))
