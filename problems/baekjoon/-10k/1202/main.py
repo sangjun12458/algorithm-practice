5b73b7c2ffab79ac657208751c012f22452ee0b1
sangjun12458 <sjpark514@khu.ac.kr>
2026-01-09 14:41:55 +0900

    fix(boj): complete solution for 1202 - solved TLE using heap

--- problems/baekjoon/-10k/1202/main.py
+++ problems/baekjoon/-10k/1202/main.py
@@ -1,24 +1,24 @@
 # 1202. 보석 도둑
 import sys
+import heapq
+
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
 jewel = [tuple(map(int, input().split())) for _ in range(N)]
 bag = [int(input()) for _ in range(K)]
-visited = [0] * K
 
-jewel.sort(key = lambda x: -x[1])
+jewel.sort()
 bag.sort()
 
+heap = []
 answer = []
-for i in range(N):
-    weight = jewel[i][0]
-    value = jewel[i][1]
-
-    for j in range(K):
-        if not visited[j] and weight <= bag[j]:
-            visited[j] = True
-            answer.append(value)
-            break
+pj = 0
+for pb in range(K):
+    while pj < N and jewel[pj][0] <= bag[pb]:
+        heapq.heappush(heap, -jewel[pj][1])
+        pj += 1
+    if heap:
+        answer.append(-heapq.heappop(heap))
 
 print(sum(answer))
