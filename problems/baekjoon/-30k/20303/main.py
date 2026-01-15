12cb14aff1cb73157af3062cdc0d1d14aba8c494
sangjun12458 <sjpark514@khu.ac.kr>
2026-01-15 21:29:39 +0900

    feat(boj): set up input parsing

---
+++ problems/baekjoon/-30k/20303/main.py
@@ -0,0 +1,12 @@
+# 20303. 할로윈의 양아치
+
+N, M, K = map(int, input().split())
+candy = [0] + list(map(int, input().split()))
+friend = [[] for _ in range(N + 1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    friend[a].append(b)
+    friend[b].append(a)
+
+print(candy)
+print(friend)
