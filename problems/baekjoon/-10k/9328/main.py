2a5ad2fda71a73f1226db5d6dc74bb4cf65bcc7d
sangjun12458 <sjpark514@khu.ac.kr>
2026-01-25 23:14:46 +0900

    feat(boj): add BFS start logic to previous implementation for 9328

--- problems/baekjoon/-10k/9328/main.py
+++ problems/baekjoon/-10k/9328/main.py
@@ -1,5 +1,6 @@
 # 9328. 열쇠
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 T = int(input())
@@ -8,3 +9,16 @@ for _ in range(T):
     building = [input().strip() for _ in range(h)]
     keys = input().strip()
     
+    visited = [[False] * w for _ in range(h)]
+    q = deque()
+    door = []
+    for i in range(h):
+        for j in range(w):
+            if i != 0 and i != h-1 and j != 0 and j != w-1:
+                continue
+            q.append(i, j)
+            while q:
+                x, y = q.popleft()
+                if visited[x][y]: continue
+                visited[x][y] = True
+                
