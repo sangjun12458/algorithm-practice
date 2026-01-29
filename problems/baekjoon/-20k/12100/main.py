870709be706eab14066910798b3bb3d0239d045b
sangjun12458 <sjpark514@khu.ac.kr>
2026-01-29 19:28:50 +0900

    fix(boj): complete solution for 12100

--- problems/baekjoon/-20k/12100/main.py
+++ problems/baekjoon/-20k/12100/main.py
@@ -1,5 +1,6 @@
 # 2048 (Easy)
 import sys
+import copy
 sys.setrecursionlimit(int(1e5))
 
 DIR = ((0, 1), (0, -1), (1, 0), (-1, 0))
@@ -7,7 +8,7 @@ DIR = ((0, 1), (0, -1), (1, 0), (-1, 0))
 N = int(input())
 board = [list(map(int, input().split())) for _ in range(N)]
 
-def move(r, c, dr, dc, board):
+def move(r, c, dr, dc, board, visited):
     while 0 <= r + dr < N and 0 <= c + dc < N:
         nr, nc = r + dr, c + dc
         if board[nr][nc] == 0:
@@ -15,13 +16,18 @@ def move(r, c, dr, dc, board):
             r += dr
             c += dc
         elif board[r][c] == board[nr][nc]:
+            if visited[r][c] or visited[nr][nc]:
+                break
             board[r][c], board[nr][nc] = 0, 2*board[r][c]
+            visited[r][c] = False
+            visited[nr][nc] = True
             r += dr
             c += dc
         else:
             break
 
 def slide(dr, dc, board):
+    visited = [[False] * N for _ in range(N)]
     r_start = N-1 if dr == 1 else 0
     r_end = -1 if dr == 1 else N
     c_start = N-1 if dc == 1 else 0
@@ -30,18 +36,19 @@ def slide(dr, dc, board):
         for c in range(c_start, c_end, -dc if dc else 1):
             if board[r][c] == 0:
                 continue
-            move(r, c, dr, dc, board)
-
-    return board
+            move(r, c, dr, dc, board, visited)
 
 def dfs(depth, board):
     if depth == 5:
-        answer = 0
+        global answer
         for i in range(N):
             answer = max(answer, max(board[i]))
         return
     for dr, dc in DIR:
-        new_board = slide(dr, dc, board[:])
-        dfs(depth+1, new_board)
+        nBoard = copy.deepcopy(board)
+        slide(dr, dc, nBoard)
+        dfs(depth+1, nBoard)
 
-dfs(0, board[:])
+answer = 0
+dfs(0, board)
+print(answer)
