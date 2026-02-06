8188f4b249b94f103200c98a9efa2b2f093b3685
sangjun12458 <sjpark514@khu.ac.kr>
2026-02-06 23:24:36 +0900

    feat(boj): incomlete solution for 3190 - some test case failed

--- problems/baekjoon/-10k/3190/main.py
+++ problems/baekjoon/-10k/3190/main.py
@@ -1,7 +1,8 @@
 # 3190. 뱀
+from collections import deque
 
 UP, RIGHT, DOWN, LEFT = 0, 1, 2, 3
-DIR = ((-1, 0), (1, 0), (0, -1), (0, 1))
+DIR = ((-1, 0), (0, 1), (1, 0), (0, -1))
 MAX_LEN = 10000
 
 N = int(input())
@@ -11,33 +12,46 @@ for _ in range(K):
     r, c = map(int, input().split())
     board[r-1][c-1] = 1
 L = int(input())
-rots = [''] * (MAX_LEN + 1)
+rots = [0] * (MAX_LEN + 1)
 for _ in range(L):
     X, C = input().split()
-    rots[int(X)] = C
+    rots[int(X)] = -1 if C == 'L' else 1
 
-snake = [(0, 0, RIGHT)] # list -> linked list
+head = (0, 0, RIGHT)
+board[head[0]][head[1]] = -1
+body = deque() # list -> linked list
 game_over = False
 tick = 0
 while not game_over:
+    # print()
+    # print(tick)
+    # for row in board:
+    #     print(row)
+    # print()
+
     tick += 1
     if tick > N + MAX_LEN:
         game_over = True
         continue
 
-    x, y, (dx, dy) = snake[0]
+    x, y, d = head
+    dx, dy = DIR[d]
     nx, ny = x + dx, y + dy
     if not (0 <= nx < N and 0 <= ny < N):
         game_over = True
         continue
 
+    head = (nx, ny, (d+rots[tick])%4)
+    body.appendleft((x, y))
     if board[nx][ny] == -1:
-        game_over = True
-    elif board[nx][ny] == 0:
-        pass
-    elif board[nx][ny] == 1:
-        pass
+        tx, ty = body.pop()
+        board[tx][ty] = 0
+        if board[nx][ny] == -1:
+            game_over = True
+    else:
+        if board[nx][ny] == 0:
+            tx, ty = body.pop()
+            board[tx][ty] = 0
+    board[nx][ny] = -1
 
-    # for x, y, dx, dy in snake:
-    #     nx, ny = x + dx, y + dy
-        
+print(tick)
