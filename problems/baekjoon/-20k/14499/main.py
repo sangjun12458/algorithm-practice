48d3d115f9378d24f7931f501a3ccc9609a303aa
sangjun12458 <sjpark514@khu.ac.kr>
2026-02-07 21:45:21 +0900

    feat(boj): complete solution for 14499

--- problems/baekjoon/-20k/14499/main.py
+++ problems/baekjoon/-20k/14499/main.py
@@ -4,9 +4,33 @@ N, M, x, y, K = map(int, input().split())
 map_for_dice = []
 for _ in range(N):
     map_for_dice.append(list(map(int, input().split())))
-seq = list(map(int, input().split()))
+dirs = list(map(int, input().split())) # 동 1, 서 2, 북 3, 남 4
+delta = ((0, 0), (0, 1), (0, -1), (-1, 0), (1, 0))
+dice = [0] * 7
+#   2
+# 4 1 3
+#   5
+#   6
 
-for move in seq:
-    pass
+for dir in dirs:
+    dx, dy = delta[dir]
+    nx, ny = x + dx, y + dy
+    if not (0 <= nx < N and 0 <= ny < M):
+        continue
 
-print(map_for_dice)
+    if dir == 1:
+        dice[1], dice[3], dice[4], dice[6] = dice[3], dice[6], dice[1], dice[4]
+    elif dir == 2:
+        dice[1], dice[3], dice[4], dice[6] = dice[4], dice[1], dice[6], dice[3]
+    elif dir == 3:
+        dice[1], dice[2], dice[5], dice[6] = dice[2], dice[6], dice[1], dice[5]
+    elif dir == 4:
+        dice[1], dice[2], dice[5], dice[6] = dice[5], dice[1], dice[6], dice[2]
+
+    if map_for_dice[nx][ny] == 0:
+        map_for_dice[nx][ny] = dice[1]
+    else:
+        dice[1] = map_for_dice[nx][ny]
+        map_for_dice[nx][ny] = 0        
+    x, y = nx, ny
+    print(dice[6])
