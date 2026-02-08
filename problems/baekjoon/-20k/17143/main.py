6595c993f20fb6b47d6497ff0052cbf75f92a730
sangjun12458 <sjpark514@khu.ac.kr>
2026-02-08 22:57:32 +0900

    feat(boj): Add fisherman movement

--- problems/baekjoon/-20k/17143/main.py
+++ problems/baekjoon/-20k/17143/main.py
@@ -1,10 +1,24 @@
 # 17143. 낚시왕
 
 R, C, M = map(int, input().split())
-board = [[0] * (C+1) for _ in range(R+1)]
+board = [[False] * (C+1) for _ in range(R+1)]
 sharks = []
 for _ in range(M):
     r, c, s, d, z = map(int, input().split())
-    board[r][c] = 1
+    board[r][c] = True
     sharks.append((r, c, s, d, z))
+
+ans = 0
+for fisher in range(1, C+1):
+    # catch a shark
+    for row in range(1, R+1):
+        if board[row][fisher]:
+            
+
+    # sharks move
+    for r, c, s, d, z in sharks:
+        # need to calculate next pos
+        nr = r
+        nc = c
+
     
