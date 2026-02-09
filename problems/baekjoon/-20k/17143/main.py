e1cdcd7a4099b40027303258920e4ddf2ff1bc30
sangjun12458 <sjpark514@khu.ac.kr>
2026-02-09 23:49:24 +0900

    feat(boj): Add shark catching and shark movements

--- problems/baekjoon/-20k/17143/main.py
+++ problems/baekjoon/-20k/17143/main.py
@@ -1,24 +1,53 @@
 # 17143. 낚시왕
 
+DIR = ((0, 0), (-1, 0), (1, 0), (0, 1), (0, -1)) 
+### 1. 위, 2. 아래, 3. 오른쪽, 4. 왼쪽
+
 R, C, M = map(int, input().split())
-board = [[False] * (C+1) for _ in range(R+1)]
+board = [[-1] * (C+1) for _ in range(R+1)]
 sharks = []
-for _ in range(M):
+catched = [False] * 10001
+
+for i in range(M):
     r, c, s, d, z = map(int, input().split())
-    board[r][c] = True
+    board[r][c] = i
     sharks.append((r, c, s, d, z))
 
 ans = 0
 for fisher in range(1, C+1):
     # catch a shark
     for row in range(1, R+1):
-        if board[row][fisher]:
-            
+        shark_idx = board[row][fisher]
+        if shark_idx != -1 and not catched:
+            ans += sharks[shark_idx][4]
+            catched[shark_idx] = True
+            board[row][fisher] = -1
+            break
 
     # sharks move
-    for r, c, s, d, z in sharks:
+    for idx, (r, c, s, d, z) in enumerate(sharks):
         # need to calculate next pos
-        nr = r
-        nc = c
+        dr, dc = DIR[d]
+        nr, nc = r + s*dr, c + s*dc
+        nd = d
+        if not (0 < nr <= R):
+            nr = 2 * R - nr if nr > R else -nr
+            nd = (d + 1) % 2 + 1
+        if not (0 < nc <= C):
+            nc = 2 * C - nc if nc > C else -nc
+            nd = (d + 1) % 2 + 3
+            print(nr, nc)
+        # check direction
+        sharks[idx] = (nr, nc, s, nd, z)
+        if board[r][c] == idx:
+            board[r][c] = -1
+        if board[nr][nc] != -1:
+            other_idx = board[nr][nc]
+            other_z = sharks[other_idx][4]
+            if z > other_z:
+                board[nr][nc] = idx
+                catched[other_idx] = True
+            else:
+                catched[idx] = True
 
-    
+print(ans)
