77b08671ffb2e9dfecfced8a48113289b6782857
sangjun12458 <sjpark514@khu.ac.kr>
2026-02-15 23:58:09 +0900

    feat(boj): modify the input code and add rotation

--- problems/baekjoon/-20k/14891/main.py
+++ problems/baekjoon/-20k/14891/main.py
@@ -1,6 +1,16 @@
 # 14891. 톱니바퀴
-
-gears = [input().strip() for _ in range(4)]
+from collections import deque
+gears = [deque(input().strip()) for _ in range(4)] # 오른쪽 2, 왼쪽 6
 K = int(input())
-for _ in range(K):
-    n, d = map(int, input().split())
+seq = [tuple(map(int, input().split())) for _ in range(K)] # 시계 1, 반시계 -1
+
+def turn(number, clockwise):
+    if clockwise == 1:
+        gears[number].appendleft(gears[number].pop())
+    else:
+        gears[number].append(gears[number].popleft())
+
+for n, d in seq:
+    left, right = gears[n][6], gears[n][2]
+    turn(n, d)
+    
