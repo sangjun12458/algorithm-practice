8c10332bc0f8e3b38c8305ab6252fd0ebac0b3d4
sangjun12458 <sjpark514@khu.ac.kr>
2026-02-20 23:44:23 +0900

    feat(boj): handle input for 16235

--- problems/baekjoon/-20k/13975/main.py
+++ problems/baekjoon/-20k/13975/main.py
@@ -8,11 +8,19 @@ t = int(input().strip())
 for _ in range(t):
     k = int(input().strip())
     chapters = list(map(int, input().split()))
-    groups = []
+    files = []
     result = 0
 
     for i in range(k):
-        heapq.heappush(groups, (chapters[i], i))
+        heapq.heappush(files, (chapters[i], i, i))
+
+    for _ in range(k-1):
+        f1 = heapq.heappop(files)
+        while f1[2] != files[0][1] + 1 and files[0][2] != f1[1]:
+            heapq
+
+    print(groups)
+
 
         
     print(result)
---
+++ problems/baekjoon/-20k/16235/main.py
@@ -0,0 +1,5 @@
+# 16235. 나무 재테크
+
+N, M, K = map(int, input().split())
+land = [tuple(map(int, input().split())) for _ in range(N)]
+trees = [tuple(map(int, input().split())) for _ in range(M)]
