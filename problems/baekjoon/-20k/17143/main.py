241a6805b679cd716b3aedf72414810c43dd3113
sangjun12458 <sjpark514@khu.ac.kr>
2026-02-23 15:16:56 +0900

    fix(boj): complete solution for 17143

--- problems/baekjoon/-20k/17143/main.py
+++ problems/baekjoon/-20k/17143/main.py
@@ -1,7 +1,7 @@
 # 17143. 낚시왕
 
 DIR = ((0, 0), (-1, 0), (1, 0), (0, 1), (0, -1)) 
-### 1. 위, 2. 아래, 3. 오른쪽, 4. 왼쪽
+# 1. 위, 2. 아래, 3. 오른쪽, 4. 왼쪽
 
 R, C, M = map(int, input().split())
 board = [[-1] * (C) for _ in range(R)]
@@ -15,7 +15,7 @@ for i in range(M):
 
 ans = 0
 for fisher in range(C):
-    # catch a shark
+    # 낚시
     for row in range(R):
         shark_idx = board[row][fisher]
         if shark_idx != -1 and not catched[shark_idx]:
@@ -24,12 +24,14 @@ for fisher in range(C):
             board[row][fisher] = -1
             break
 
-    next_pos = [[] for _ in range] # 이전 위치, 새로운 위치 모두 구한 후 갱신
-    # sharks move
+    # 상어 이동
     for idx, (r, c, s, d, z) in enumerate(sharks):
         if catched[idx]: continue
-        dr, dc = DIR[d]
 
+        if board[r][c] == idx:
+            board[r][c] = -1
+
+        dr, dc = DIR[d] 
         if d == 1:
             nr = 2 * R - 2 - r - s*dr
         else:
@@ -54,17 +56,16 @@ for fisher in range(C):
                 nd = 4
 
         sharks[idx] = (nr, nc, s, nd, z)
-    
-        board[r][c] = -1
-        if board[nr][nc] != -1:
-            other_idx = board[nr][nc]
-            other_z = sharks[other_idx][4]
-            if z > other_z:
-                board[nr][nc] = idx
-                catched[other_idx] = True
-            else:
-                catched[idx] = True
-        else:
+
+        if board[nr][nc] == -1 or board[nr][nc] >= idx:
             board[nr][nc] = idx
+            continue
+        other_idx = board[nr][nc]
+        other_z = sharks[other_idx][4]
+        if z > other_z:
+            board[nr][nc] = idx
+            catched[other_idx] = True
+        else:
+            catched[idx] = True
 
 print(ans)
