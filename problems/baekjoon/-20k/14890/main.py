cb7bff613fe4e9a3e87ee1b96852cdfe4b599794
sangjun12458 <sjpark514@khu.ac.kr>
2026-02-25 23:46:20 +0900

    feat(boj): Add checking ramp function for 14890

--- problems/baekjoon/-20k/14890/main.py
+++ problems/baekjoon/-20k/14890/main.py
@@ -1,8 +1,39 @@
 # 14890. 경사로
+from collections import deque
 
 N, L = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
+answer = 0
 
 for i in range(N):
-    for j in range(N):
-        start = j
+    ramp = deque()
+    passable = True
+    for j in range(N):            
+        if not ramp:
+            ramp.append(j)
+            continue
+        h = arr[i][ramp[0]]
+        nh = arr[i][j]
+        if nh == h:
+            ramp.append(j)
+            if len(ramp) > L:
+                ramp.popleft()
+        elif nh > h:
+            if nh + 1 == h and len(ramp) == L:
+                ramp.clear()
+                ramp.append(j)
+            else:
+                passable = False
+                break
+        else:
+            if nh - 1 == h:
+                ramp.clear()
+                ramp.append(j)
+            else:
+                passable = False
+                break
+    if passable:
+        print(arr[i])
+        answer += 1
+
+print(answer)
