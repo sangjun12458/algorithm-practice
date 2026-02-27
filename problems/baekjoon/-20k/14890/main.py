f46f00f02d6832b28333c782bf5925e9e1d83bc1
sangjun12458 <sjpark514@khu.ac.kr>
2026-02-27 22:27:45 +0900

    feat(boj): complete solution for 14890

--- problems/baekjoon/-20k/14890/main.py
+++ problems/baekjoon/-20k/14890/main.py
@@ -1,40 +1,72 @@
 # 14890. 경사로
-from collections import deque
-
 N, L = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 answer = 0
 
 for i in range(N):
-    ramp = deque()
+    ramp_h = arr[i][0]
+    ramp_l = 1
     passable = True
-    for j in range(N):            
-        if not ramp:
-            ramp.append(j)
-            continue
-        h = arr[i][ramp[0]]
-        nh = arr[i][j]
-
-        if nh == h + 1:
-            if len(ramp) == L:
-                ramp.clear()
-                ramp.append(j)
+    for j in range(1, N):
+        h = arr[i][j]
+        if h + 1 == ramp_h:
+            if ramp_l >= 0:
+                ramp_h = h
+                ramp_l = 1 - L
             else:
                 passable = False
                 break
-        elif nh == h:
-            ramp.append(j)
-            if len(ramp) > L:
-                ramp.popleft()
-        elif nh == h - 1:
-            ramp.clear()
-            ramp.append(j)
+        elif h == ramp_h:
+            ramp_l += 1
+        elif h - 1 == ramp_h:
+            if ramp_l >= L:
+                ramp_l = 1
+                ramp_h = h
+            else:
+                passable = False
+                break
         else:
             passable = False
             break
+    if ramp_l < 0:
+        passable = False
 
     if passable:
-        print(arr[i])
+        # print(arr[i])
+        answer += 1
+
+for j in range(N):
+    ramp_h = arr[0][j]
+    ramp_l = 1
+    passable = True
+    for i in range(1, N):
+        h = arr[i][j]
+        if h + 1 == ramp_h:
+            if ramp_l >= 0:
+                ramp_h = h
+                ramp_l = 1 - L
+            else:
+                passable = False
+                break
+        elif h == ramp_h:
+            ramp_l += 1
+        elif h - 1 == ramp_h:
+            if ramp_l >= L:
+                ramp_l = 1
+                ramp_h = h
+            else:
+                passable = False
+                break
+        else:
+            passable = False
+            break
+    if ramp_l < 0:
+        passable = False
+
+    if passable:
+        # print()
+        # for row in arr:
+        #     print(row[j])
         answer += 1
 
 print(answer)
