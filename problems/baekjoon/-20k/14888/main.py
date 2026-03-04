a10952ea73320525d307de0a76ed20da0f26aeab
sangjun12458 <sjpark514@khu.ac.kr>
2026-03-04 23:59:28 +0900

    feat(boj): DFS for 14888

--- problems/baekjoon/-20k/14888/main.py
+++ problems/baekjoon/-20k/14888/main.py
@@ -6,4 +6,18 @@ O = list(map(int, input().split())) # +, -, *, /
 
 arr = [[0]]
 
+stack = [(A[0], O.copy())]
+
+while stack:
+    x, opers = stack.pop()
+    idx = sum(opers)
+    if opers[0] > 0:
+        pass
+    if opers[1] > 0:
+        pass
+    if opers[2] > 0:
+        pass
+    if opers[3] > 0:
+        pass
+
 answer = 0
