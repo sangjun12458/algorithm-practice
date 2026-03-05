5c494db8d044685889d10eab0102a870facf0c7c
sangjun12458 <sjpark514@khu.ac.kr>
2026-03-05 10:33:02 +0900

    fix(boj): complete solution for 14888

--- problems/baekjoon/-20k/14888/main.py
+++ problems/baekjoon/-20k/14888/main.py
@@ -1,23 +1,33 @@
-# 14888.
+# 14888. 연산자 끼워넣기
+INF = 1_000_000_000
 
 N = int(input())
 A = list(map(int, input().split()))
 O = list(map(int, input().split())) # +, -, *, /
 
 arr = [[0]]
-
 stack = [(A[0], O.copy())]
+max_v = -INF
+min_v = INF
 
 while stack:
-    x, opers = stack.pop()
-    idx = sum(opers)
-    if opers[0] > 0:
-        pass
-    if opers[1] > 0:
-        pass
-    if opers[2] > 0:
-        pass
-    if opers[3] > 0:
-        pass
+    x, (a, b, c, d) = stack.pop()
+    s = sum([a, b, c, d])
+    if s == 0:
+        max_v = max(x, max_v)
+        min_v = min(x, min_v)
+        continue
 
-answer = 0
+    y = A[N - s]
+    if a > 0:
+        stack.append((x + y, [a - 1, b, c, d]))
+    if b > 0:
+        stack.append((x - y, [a, b - 1, c, d]))
+    if c > 0:
+        stack.append((x * y, [a, b, c - 1, d]))
+    if d > 0:
+        z = abs(x) // abs(y)
+        stack.append((z if x * y > 0 else -z, [a, b, c, d - 1]))
+    
+print(max_v)
+print(min_v)
