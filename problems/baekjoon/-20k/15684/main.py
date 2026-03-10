4eefd909ac0137e1c1c9512b15f62d4d7aeee811
sangjun12458 <sjpark514@khu.ac.kr>
2026-03-10 23:15:06 +0900

    feat(boj): Add a function to check if the ladder is corret

--- problems/baekjoon/-20k/15684/main.py
+++ problems/baekjoon/-20k/15684/main.py
@@ -1,7 +1,31 @@
 # 15684. 사다리 조작
 
+def check():
+    global ladder
+    for i in range(N):
+        col = i
+        row = 0
+        passed = False
+        while row < H:
+            if ladder[row][col] == 0 or passed:
+                passed = False
+            else:
+                col += ladder[row][col]
+                passed = True
+            row += 1
+        if col != i:
+            return False 
+    return True
+
+def dfs():
+    return
+
 N, M, H = map(int, input().split())
-lines = []
+ladder = [[0] * N for _ in range(H)]
 for _ in range(M):
     a, b = map(int, input().split())
-    lines.append((a, b))
+    ladder[a-1][b-1] = 1
+    ladder[a-1][b] = -1
+
+for row in ladder:
+    print(*row)
