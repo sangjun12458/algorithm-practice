2bf5949605658026673aee463b53b127459ce1ba
sangjun12458 <sjpark514@khu.ac.kr>
2026-03-11 23:18:04 +0900

    feat(boj): incomplete solution for 15684 - some test cases failed with TLE

--- problems/baekjoon/-20k/15684/main.py
+++ problems/baekjoon/-20k/15684/main.py
@@ -5,20 +5,34 @@ def check():
     for i in range(N):
         col = i
         row = 0
-        passed = False
         while row < H:
-            if ladder[row][col] == 0 or passed:
-                passed = False
-            else:
+            if ladder[row][col] != 0:
                 col += ladder[row][col]
-                passed = True
             row += 1
         if col != i:
-            return False 
+            return False
     return True
 
-def dfs():
-    return
+def dfs(depth, r, c):
+    global answer, ladder
+    if depth > 3:
+        return
+    if check():
+        answer = min(depth, answer)
+        return
+    for i in range(r, H):
+        for j in range(N-1):
+            if i == r and j < c:
+                continue
+            if ladder[i][j] != 0:
+                continue
+            if ladder[i][j+1] != 0:
+                continue
+            ladder[i][j] = 1
+            ladder[i][j+1] = -1
+            dfs(depth+1, i, j)
+            ladder[i][j] = 0
+            ladder[i][j+1] = 0
 
 N, M, H = map(int, input().split())
 ladder = [[0] * N for _ in range(H)]
@@ -27,5 +41,6 @@ for _ in range(M):
     ladder[a-1][b-1] = 1
     ladder[a-1][b] = -1
 
-for row in ladder:
-    print(*row)
+answer = 10
+dfs(0, 0, 0)
+print(answer if answer <= 3 else -1)
