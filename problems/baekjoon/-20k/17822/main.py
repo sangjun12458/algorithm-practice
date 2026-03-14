4622977cce381a9e28b904ae709099c4f834dc57
sangjun12458 <sjpark514@khu.ac.kr>
2026-03-14 23:11:33 +0900

    feat(boj): Add disk rotation for 17822

--- problems/baekjoon/-20k/17822/main.py
+++ problems/baekjoon/-20k/17822/main.py
@@ -1,9 +1,12 @@
 # 17822. 원판 돌리기
-
 N, M, T = map(int, input().split())
 disk = []
+ptr = [0] * N
 for _ in range(N):
     disk.append(list(map(int, input().split())))
 for _ in range(T):
     x, d, k = map(int, input().split())
-    
+
+    if d == 0: d = -1
+    for i in range(x-1, N, x):
+        ptr[i] = (ptr[i] + d * k) % 4
