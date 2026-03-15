9937cc9e2ca67d4ba1e13f1db6f9e9829f5cab9b
sangjun12458 <sjpark514@khu.ac.kr>
2026-03-15 23:58:32 +0900

    feat(boj): Add deleting near same numbaer on disk for 17822

--- problems/baekjoon/-20k/17822/main.py
+++ problems/baekjoon/-20k/17822/main.py
@@ -6,7 +6,34 @@ for _ in range(N):
     disk.append(list(map(int, input().split())))
 for _ in range(T):
     x, d, k = map(int, input().split())
-
+    print(disk, ptr)
     if d == 0: d = -1
     for i in range(x-1, N, x):
-        ptr[i] = (ptr[i] + d * k) % 4
+        ptr[i] = (ptr[i] + d * k) % M
+    print(disk, ptr)
+    for i in range(len(disk)):
+        for j in range(M):
+            arr = [(i, j)]
+            while arr:
+                u, v = arr.pop()
+                value = disk[u][(ptr[u] + v) % M]
+                if value == 0:
+                    continue
+                disk[u][(ptr[u] + v) % M] = 0
+                print(value)
+                if disk[u][(ptr[u] + v - 1) % M] == value:
+                    print(u, ptr[u] + v - 1)
+                    arr.append((u, v - 1))
+                if disk[u][(ptr[u] + v + 1) % M] == value:
+                    print(u, ptr[u] + v + 1)
+                    arr.append((u, v + 1))
+                if u > 0:
+                    if disk[u-1][(ptr[u-1] + v) % M] == value:
+                        print(u-1, (ptr[u-1] + v) % M)
+                        arr.append((u-1, v))
+                if u < M-1:
+                    if disk[u+1][(ptr[u+1] + v) % M] == value:
+                        print(u+1, (ptr[u+1] + v) % M)
+                        arr.append((u+1, v))
+    print(disk)
+    print()
