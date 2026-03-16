36a3fdfae54504ca5d149731264af42916026577
sangjun12458 <sjpark514@khu.ac.kr>
2026-03-16 23:59:43 +0900

    17822

--- problems/baekjoon/-20k/17822/main.py
+++ problems/baekjoon/-20k/17822/main.py
@@ -6,19 +6,20 @@ for _ in range(N):
     disk.append(list(map(int, input().split())))
 for _ in range(T):
     x, d, k = map(int, input().split())
-    print(disk, ptr)
     if d == 0: d = -1
     for i in range(x-1, N, x):
         ptr[i] = (ptr[i] + d * k) % M
-    print(disk, ptr)
     for i in range(len(disk)):
         for j in range(M):
+            print(disk)
             arr = [(i, j)]
+            cnt = 0
             while arr:
                 u, v = arr.pop()
                 value = disk[u][(ptr[u] + v) % M]
                 if value == 0:
                     continue
+                cnt += 1
                 disk[u][(ptr[u] + v) % M] = 0
                 print(value)
                 if disk[u][(ptr[u] + v - 1) % M] == value:
@@ -35,5 +36,7 @@ for _ in range(T):
                     if disk[u+1][(ptr[u+1] + v) % M] == value:
                         print(u+1, (ptr[u+1] + v) % M)
                         arr.append((u+1, v))
+            if cnt == 1:
+                
     print(disk)
     print()
