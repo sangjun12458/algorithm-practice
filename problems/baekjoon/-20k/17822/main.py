f0a558120dff3bf28fd91d541cb2d11d9157c902
sangjun12458 <sjpark514@khu.ac.kr>
2026-03-17 23:59:06 +0900

    feat(boj): incomplete solution for 17822 - some test cases failed

--- problems/baekjoon/-20k/17822/main.py
+++ problems/baekjoon/-20k/17822/main.py
@@ -1,42 +1,39 @@
 # 17822. 원판 돌리기
+
 N, M, T = map(int, input().split())
 disk = []
 ptr = [0] * N
 for _ in range(N):
     disk.append(list(map(int, input().split())))
+    
 for _ in range(T):
     x, d, k = map(int, input().split())
     if d == 0: d = -1
     for i in range(x-1, N, x):
         ptr[i] = (ptr[i] + d * k) % M
+    arr = []
     for i in range(len(disk)):
         for j in range(M):
-            print(disk)
-            arr = [(i, j)]
-            cnt = 0
-            while arr:
-                u, v = arr.pop()
-                value = disk[u][(ptr[u] + v) % M]
-                if value == 0:
-                    continue
-                cnt += 1
-                disk[u][(ptr[u] + v) % M] = 0
-                print(value)
-                if disk[u][(ptr[u] + v - 1) % M] == value:
-                    print(u, ptr[u] + v - 1)
-                    arr.append((u, v - 1))
-                if disk[u][(ptr[u] + v + 1) % M] == value:
-                    print(u, ptr[u] + v + 1)
-                    arr.append((u, v + 1))
-                if u > 0:
-                    if disk[u-1][(ptr[u-1] + v) % M] == value:
-                        print(u-1, (ptr[u-1] + v) % M)
-                        arr.append((u-1, v))
-                if u < M-1:
-                    if disk[u+1][(ptr[u+1] + v) % M] == value:
-                        print(u+1, (ptr[u+1] + v) % M)
-                        arr.append((u+1, v))
-            if cnt == 1:
-                
-    print(disk)
-    print()
+            value = disk[i][(ptr[i] + j) % M]
+            if value == 0:
+                continue
+            check = False
+            if disk[i][(ptr[i] + j - 1) % M] == value:
+                check = True
+            if disk[i][(ptr[i] + j + 1) % M] == value:
+                check = True
+            if i > 0:
+                if disk[i-1][(ptr[i-1] + j) % M] == value:
+                    check = True
+            if i < M-1:
+                if disk[i+1][(ptr[i+1] + j) % M] == value:
+                    check =True
+            if check:
+                arr.append((i, j))
+    for u, v in arr:
+        disk[u][(ptr[u] + v) % M] = 0
+
+answer = 0
+for t in disk:
+    answer += sum(t)
+print(answer)
