7067b154e4b0937f11fa16ad150d0a039689eb29
sangjun12458 <sjpark514@khu.ac.kr>
2026-03-18 15:39:14 +0900

    feat(boj): complete solution for 17822

--- problems/baekjoon/-20k/17822/main.py
+++ problems/baekjoon/-20k/17822/main.py
@@ -1,39 +1,59 @@
 # 17822. 원판 돌리기
 
 N, M, T = map(int, input().split())
-disk = []
+disks = []
 ptr = [0] * N
 for _ in range(N):
-    disk.append(list(map(int, input().split())))
-    
+    disks.append(list(map(int, input().split())))
+
 for _ in range(T):
     x, d, k = map(int, input().split())
     if d == 0: d = -1
     for i in range(x-1, N, x):
         ptr[i] = (ptr[i] + d * k) % M
     arr = []
-    for i in range(len(disk)):
+    for i in range(len(disks)):
         for j in range(M):
-            value = disk[i][(ptr[i] + j) % M]
+            value = disks[i][(ptr[i] + j) % M]
             if value == 0:
                 continue
             check = False
-            if disk[i][(ptr[i] + j - 1) % M] == value:
+            if disks[i][(ptr[i] + j - 1) % M] == value:
                 check = True
-            if disk[i][(ptr[i] + j + 1) % M] == value:
+            if disks[i][(ptr[i] + j + 1) % M] == value:
                 check = True
             if i > 0:
-                if disk[i-1][(ptr[i-1] + j) % M] == value:
+                if disks[i-1][(ptr[i-1] + j) % M] == value:
                     check = True
-            if i < M-1:
-                if disk[i+1][(ptr[i+1] + j) % M] == value:
-                    check =True
+            if i < N-1:
+                if disks[i+1][(ptr[i+1] + j) % M] == value:
+                    check = True
             if check:
                 arr.append((i, j))
-    for u, v in arr:
-        disk[u][(ptr[u] + v) % M] = 0
+    if arr:
+        for u, v in arr:
+            disks[u][(ptr[u] + v) % M] = 0
+        continue
+
+    total = 0
+    cnt = 0
+    for disk in disks:
+        for num in disk:
+            if num == 0:
+                continue
+            total += num
+            cnt += 1
+    if cnt == 0:
+        continue
+    avg = total / cnt
+    for i in range(N):
+        for j in range(M):            
+            if 0 < disks[i][j] < avg:
+                disks[i][j] += 1
+            elif disks[i][j] > avg:
+                disks[i][j] -= 1
 
 answer = 0
-for t in disk:
-    answer += sum(t)
+for disk in disks:
+    answer += sum(disk)
 print(answer)
