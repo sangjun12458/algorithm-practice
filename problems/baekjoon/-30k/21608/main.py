877f3b0f28ad6042c8027f9f9ba9a4ed0ce09b16
sangjun12458 <sjpark514@khu.ac.kr>
2026-03-19 23:34:27 +0900

    fix(boj): modify input for 21608

--- problems/baekjoon/-30k/21608/main.py
+++ problems/baekjoon/-30k/21608/main.py
@@ -1,7 +1,11 @@
-# 21608. 
+# 21608. 상어 초등학교
 import sys
 input = sys.stdin.readline
 
 N = int(input())
+preferred = [0] * N * N
 for _ in range(N*N):
-    arr = list(map(int, input().split()))
+    x, y1, y2, y3, y4 = map(int, input().split())
+    preferred[x-1] = (y1, y2, y3, y4)
+
+print(preferred)
