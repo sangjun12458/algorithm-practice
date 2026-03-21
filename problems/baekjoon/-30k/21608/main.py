e07e236b275be7cb5be8038087fe28b61f2f1383
sangjun12458 <sjpark514@khu.ac.kr>
2026-03-21 23:38:09 +0900

    feat(boj-21608): calculate total of all student satisfaction

--- problems/baekjoon/-30k/21608/main.py
+++ problems/baekjoon/-30k/21608/main.py
@@ -3,18 +3,20 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-preferred = [0] * N * N
+preferred = [0] * (N * N + 1)
 order = []
 for _ in range(N*N):
     x, y1, y2, y3, y4 = map(int, input().split())
-    preferred[x-1] = (y1, y2, y3, y4)
-    order.append(x-1)
+    preferred[x] = (y1, y2, y3, y4)
+    order.append(x)
 
-seats = [[-1] * N for _ in range(N)]
+seats = [[0] * N for _ in range(N)]
 for x in order:
     r, c, satisfaction = 0, 0, 0
     for i in range(N):
         for j in range(N):
+            if seats[i][j]:
+                continue
             cnt = 0
             for di, dj in ((0, 1), (0, -1), (1, 0), (-1, 0)):
                 ni, nj = i + di, j + dj
@@ -22,7 +24,24 @@ for x in order:
                     continue
                 if seats[ni][nj] in preferred[x]:
                     cnt += 1
-            if cnt > satisfaction or seats[r][c] != -1:
+            if cnt > satisfaction or seats[r][c]:
                 r, c, satisfaction = i, j, cnt
     seats[r][c] = x
-print(seats)
+
+for row in seats:
+    print(row)
+answer = 0
+for i in range(N):
+    for j in range(N):
+        student = seats[i][j]
+        cnt = 0
+        for di, dj in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+            ni, nj = i + di, j + dj
+            if not (0 <= ni < N and 0 <= nj < N):
+                continue
+            if seats[ni][nj] in preferred[student]:
+                cnt += 1
+        if cnt > 0:
+            answer += (10 ** (cnt-1))
+        print(answer)
+print(answer)
