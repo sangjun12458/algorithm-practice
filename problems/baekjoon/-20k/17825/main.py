0307f011d9b02f4ba0bab5524b26712dd6a274b3
sangjun12458 <sjpark514@khu.ac.kr>
2026-03-24 23:04:33 +0900

    feat(boj-17825): incomplete solution - some test cases failed

--- problems/baekjoon/-20k/17825/main.py
+++ problems/baekjoon/-20k/17825/main.py
@@ -1,7 +1,11 @@
 # 17825. 주사위 윷놀이
+import sys
+sys.setrecursionlimit(10**6)
 
 dice = tuple(map(int, input().split()))
 board = []
+piece = [0, 0, 0, 0]
+answer = 0
 
 def set_board():
     global board
@@ -16,6 +20,39 @@ def set_board():
     board.extend([(28, [27]), (27, [28]), (26, [29])]) # east: 30->28->27->26->25
     board.extend([(25, [30]), (30, [31]), (35, [20])]) # north: 25->30->35->40
 
-set_board()
+def dfs(depth, score):
+    global answer, piece
+    if depth >= 10: # 종료 조건
+        answer = max(answer, score)
+        return    
+    if score + 40 * (10-depth) <= answer: # 가지치기
+        return
+    for i in range(4):
+        if piece[i] == -1:
+            continue
+        # 다른 말과 같은 위치이면 선택하지 않도록 구현 필요
+        keep = True
+        for j in range(i):
+            if piece[j] == piece[i]:
+                keep = False
+        if not keep:
+            continue
+        # 파란칸 이동 구현 필요
+        prev_pos = piece[i]
+        cnt = 0
+        while piece[i] != -1 and cnt < dice[depth]:
+            if cnt == 0 and len(board[piece[i]][1]) == 2:
+                piece[i] = board[piece[i]][1][1]
+            else:
+                piece[i] = board[piece[i]][1][0]
+            cnt += 1
+        if piece[i] == -1:
+            dfs(depth+1, score)
+        else:
+            dfs(depth+1, score + board[piece[i]][0])
+        piece[i] = prev_pos
 
+set_board()
 print(board)
+dfs(0, 0)
+print(answer)
