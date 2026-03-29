e9da4037522a497d77350e7189ba93179f820c28
sangjun12458 <sjpark514@khu.ac.kr>
2026-03-29 23:15:50 +0900

    feat(boj-19238: flow comment

--- problems/baekjoon/-20k/19238/main.py
+++ problems/baekjoon/-20k/19238/main.py
@@ -2,5 +2,12 @@
 
 N, M, gas = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-start = list(map(int, input().split()))
+taxi = list(map(int, input().split()))
 customer = [list(map(int, input().split())) for _ in range(M)]
+
+for _ in range(M):
+    # 택시로부터 최단거리 승객 찾기 + 찾을 수 없으면 종료
+    
+    # 출발지에서 목적지까지 최단거리 + 가지 못하거나 연료가 없으면 종료
+    # 연료 충전 
+    pass
