2f7b3c762f72f35f5ff0ba429f9d925ba2d49b05
sangjun12458 <sjpark514@khu.ac.kr>
2026-04-07 23:41:45 +0900

    fix(boj-19238): error correction

--- problems/baekjoon/-20k/19238/main.py
+++ problems/baekjoon/-20k/19238/main.py
@@ -55,7 +55,8 @@ for _ in range(M):
         gas = -1
         break
     # 출발지에서 목적지까지 최단거리 + 가지 못하거나 연료가 없으면 종료
-    sr, sc, er, ec, _ = customer[target]
+    sr, sc, er, ec = customer[target]
+    v = visited[target]
     gas -= best_d
     gas_used = dist[sr*N+sc][er*N+ec]
     if gas < gas_used:
