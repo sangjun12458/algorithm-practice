7f18bbc59841604dec29b1690f9dc45d5027b982
sangjun12458 <sjpark514@khu.ac.kr>
2026-04-08 21:14:17 +0900

    fix(boj-19238): some test cases passed - change list to deque

--- problems/baekjoon/-20k/19238/main.py
+++ problems/baekjoon/-20k/19238/main.py
@@ -1,33 +1,13 @@
 # 19238. 스타트 택시
+from collections import deque
 DIR = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 N, M, gas = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 taxi_r, taxi_c = map(lambda x: int(x)-1, input().split())
-customer = [list(map(lambda x: int(x)-1, input().split()))]
+customer = [list(map(lambda x: int(x)-1, input().split())) for _ in range(M)]
+
 proccessed = [False] * N
-
-dist = [[-1] * (N*N) for _ in range(N*N)]
-for i in range(N):
-    for j in range(N):
-        if arr[i][j] == 1:
-            continue
-        start = i * N + j
-        q = [(i, j, 0)]
-        while q:
-            r, c, d = q.pop()
-            if not (0 <= r < N and 0 <= c < N):
-                continue
-            if arr[r][c] == 1:
-                continue
-            end = r * N + c
-            if dist[start][end] != -1:
-                continue
-            dist[start][end] = dist[end][start] = d
-            for dr, dc in DIR:
-                nr, nc = r + dr, c + dc
-                q.append((nr, nc, d + 1))
-
 for _ in range(M):
     # 택시로부터 최단거리 승객 찾기 + 찾을 수 없으면 종료
     target, best_d = -1, 1000
@@ -36,34 +16,52 @@ for _ in range(M):
             continue
         customer_r, customer_c, _, _ = customer[i]
         visited = [[False] * N for _ in range(N)]
-        q = [(taxi_r, taxi_c, 0)]
+        q = deque([(taxi_r, taxi_c, 0)])
         while q:
-            r, c, d = q.pop()
+            r, c, d = q.popleft()
             if r == customer_r and c == customer_c:
                 if d < best_d:
                     best_d = d
                     target = i
+                break
             if visited[r][c]:
                 continue
             visited[r][c] = True
             for dr, dc in DIR:
                 nr, nc = r + dr, c + dc
-                if 0 <= nr < N and 0 <= nc < N:
-                    q.append((nr, nc, d + 1))
-
+                if not (0 <= nr < N and 0 <= nc < N):
+                    continue
+                if arr[r][c]:
+                    continue
+                q.append((nr, nc, d + 1))
     if target == -1:
         gas = -1
         break
+    gas -= best_d
     # 출발지에서 목적지까지 최단거리 + 가지 못하거나 연료가 없으면 종료
     sr, sc, er, ec = customer[target]
-    v = visited[target]
-    gas -= best_d
-    gas_used = dist[sr*N+sc][er*N+ec]
-    if gas < gas_used:
+    visited = [[False] * N for _ in range(N)]
+    q = deque([(sr, sc, 0)])
+    gas_used = -1
+    while q:
+        r, c, d = q.popleft()
+        if r == er and c == ec:
+            gas_used = d
+            break
+        if visited[r][c]:
+            continue
+        visited[r][c] = True
+        for dr, dc in DIR:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < N and 0 <= nc < N:
+                q.append((nr, nc, d + 1))
+
+    if gas_used == -1 or gas_used > gas:
         gas = -1
         break
+
     gas += gas_used
-    taxi = [er, ec]
-    customer[target][4] = True
+    taxi_r, taxi_c = er, ec
+    proccessed[target] = True
 
 print(gas)
