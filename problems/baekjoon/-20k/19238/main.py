9eebe89de47cae66ad386fb8d68b4a341c85c531
sangjun12458 <sjpark514@khu.ac.kr>
2026-04-15 23:44:17 +0900

    fix(boj-19238): Remove duplicate BFS for passenger search

--- problems/baekjoon/-20k/19238/main.py
+++ problems/baekjoon/-20k/19238/main.py
@@ -5,59 +5,65 @@ MAX_D = 1000
 
 N, M, fuel = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-taxi_r, taxi_c = map(lambda x: int(x)-1, input().split())
+ty, tx = map(lambda x: int(x)-1, input().split())
 
 passengers = {}
 for _ in range(M):
     sy, sx, ey, ex = map(lambda x: int(x)-1, input().split())
     passengers[(sy, sx)] = (ey, ex)
 
-def cal_distance(sr, sc, end_r, end_c):
-    visited = [[-1] * N for _ in range(N)]
-    q = deque([(sr, sc)])
-    while q:
-        r, c = q.popleft()
-        dist = visited[r][c]
-
-        
-        if r == end_r and c == end_c:
-            return d
-        if visited[r][c]:
-            continue
-        visited[r][c] = True
-        for dr, dc in DIR:
-            nr, nc = r + dr, c + dc
-            if not (0 <= nr < N and 0 <= nc < N):
-                continue
-            if arr[nr][nc]:
-                continue
-            q.append((nr, nc, d + 1))
-    return MAX_D
-
-proccessed = [False] * M
 for _ in range(M):
     # 택시로부터 최단거리 승객 찾기
-    target, best_d = -1, MAX_D
-    for i in range(M):
-        if proccessed[i]:
-            continue
-        start_r, start_c, _, _ = customer[i]
-        d = cal_distance(taxi_r, taxi_c, start_r, start_c)
-        if d < best_d:
-            target = i
-            best_d = d
-    if target == -1: # 승객에게 갈 수 없으므로 종료
-        gas = -1
+    visited = [[-1] * N for _ in range(N)]
+    q = deque([(ty, tx)])
+    visited[ty][tx] = 0
+    target = None
+    while q:
+        y, x = q.popleft()
+        if passengers.get((y, x)):
+            target = (y, x)
+            break
+        for dy, dx in DIR:
+            ny, nx = y + dy, x + dx
+            if not (0 <= ny < N and 0 <= nx < N):
+                continue
+            if arr[ny][nx]:
+                continue
+            if visited[ny][nx] != -1:
+                continue
+            q.append((ny, nx))
+            visited[ny][nx] = visited[y][x] + 1
+    if target is None: # 승객에게 갈 수 없으므로 종료
+        fuel = -1
         break
-    gas -= best_d
+    fuel -= visited[target[0]][target[1]]
+
     # 출발지에서 목적지까지 최단거리
-    sr, sc, er, ec = customer[target]
-    gas_used = cal_distance(sr, sc, er, ec)
-    if gas_used == -1 or gas_used > gas: # 길이 없거나 연료가 부족하여 종료
-        gas = -1
+    ey, ex = passengers[target]
+    passengers.pop(target)
+    visited = [[-1] * N for _ in range(N)]
+    q = deque([target])
+    visited[target[0]][target[1]] = 0
+    fuel_used = float('inf')
+    while q:
+        y, x = q.popleft()
+        if y == ey and x == ex:
+            fuel_used = visited[y][x]
+            break
+        for dy, dx in DIR:
+            ny, nx = y + dy, x + dx
+            if not (0 <= ny < N and 0 <= nx < N):
+                continue
+            if arr[ny][nx]:
+                continue
+            if visited[ny][nx] != -1:
+                continue
+            q.append((ny, nx))
+
+    if fuel_used > fuel: # 길이 없거나 연료가 부족하여 종료
+        fuel = -1
         break
-    gas += gas_used
-    taxi_r, taxi_c = er, ec
-    proccessed[target] = True
+    fuel += fuel_used
+    ty, tx = ey, ex
 
 print(fuel)
