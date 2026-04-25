7380989fe049af1f493fc3b54c7eaa5987082ac1
sangjun12458 <sjpark514@khu.ac.kr>
2026-04-25 22:11:12 +0900

    (boj-15685) - 좌표를 변환하여 저장하는 방법에서 방향을 저장하는 방법으로 개선

--- problems/baekjoon/-20k/15685/main.py
+++ problems/baekjoon/-20k/15685/main.py
@@ -1,34 +1,30 @@
 # 15685. 드래곤 커브
-DIR = ((0, 1), (-1, 0), (0, -1), (1, 0))
+DIR = [(1,0),(0,-1),(-1,0),(0,1)]
 
 N = int(input())
 grid = [[0] * 101 for _ in range(101)]
 
 for _ in range(N):
     x, y, d, g = map(int, input().split())
-    dy, dx = DIR[d]
-    curves = [(y, x), (y + dy, x + dx)]
+
+    directions = [d]
 
     # 세대 커브 구현
     for _ in range(g):
-        er, ec = curves[-1]
-        l = len(curves)
-        for i in range(l-2, -1, -1):
-            pr, pc = curves[i]
-            # 기준점에 대해 회전
-            dr, dc = er - ec, er + ec
-            nr, nc = pc + dr, -pr + dc
-            if 0 <= nr <= 100 and 0 <= nc <= 100:
-                curves.append((nr, nc))
-            # else:
-            #     break
-            
-    for y, x in curves:
+        for i in range(len(directions)-1, -1, -1):
+            directions.append((directions[i] + 1) % 4)
+
+    grid[y][x] = 1
+    for dir in directions:
+        dx, dy = DIR[dir]
+        x += dx
+        y += dy
         grid[y][x] = 1
     
 answer = 0
 for i in range(100):
     for j in range(100):
-        if grid[i][j] == grid[i][j+1] == grid[i+1][j] == grid[i+1][j+1] == 1:
+        if grid[i][j] and grid[i][j+1] and grid[i+1][j] and grid[i+1][j+1]:
             answer += 1
+
 print(answer)
