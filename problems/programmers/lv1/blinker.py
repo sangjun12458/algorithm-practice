065379e83d91d3a9ef64afb58c9f1742eef7784e
sangjun12458 <sjpark514@khu.ac.kr>
2026-05-06 23:10:17 +0900

    feat(prgs-노란불 신호등): 신호등 처리 구현

--- problems/programmers/lv1/blinker.py
+++ problems/programmers/lv1/blinker.py
@@ -1,17 +1,36 @@
 
 def signals_input():
     ss = input().split(',')
-    l = []
+    arr = []
+    row = []
     for s in ss:
         a = ''
         for c in s:
             if '1' <= c <= '9':
                 a += c
         if a:
-            l.append(a)
-    signals = []
-    return l
+            row.append(int(a))
+            if len(row) >= 3:
+                arr.append(row.copy())
+                row.clear()
+    return arr
 
 signals = signals_input()
+blinkers = [[0, 0] for _ in range(len(signals))]
+answer = -1
 
-print(signals)
+for tick in range(2, 21):
+    cnt = 0
+    for x in range(len(signals)):
+        blinkers[x][1] += 1
+        if blinkers[x][1] == signals[x][blinkers[x][0]]:
+            blinkers[x][0] = (blinkers[x][0] + 1) % 3
+            blinkers[x][1] = 0
+        if blinkers[x][0] == 1:
+            cnt += 1
+    if cnt == len(signals):
+        answer = tick
+        break
+    print(blinkers)
+
+print(answer)
