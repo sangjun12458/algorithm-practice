68a99e87537e3539b95b452c7e086bb8e9714407
sangjun12458 <sjpark514@khu.ac.kr>
2026-05-08 10:27:16 +0900

    fix(programmers-노란불 신호등): complete solution

--- problems/programmers/lv1/blinker/main.py
+++ problems/programmers/lv1/blinker/main.py
@@ -1,4 +1,3 @@
-import math
 
 def signals_input():
     ss = input().split(',')
@@ -16,25 +15,26 @@ def signals_input():
                 row.clear()
     return arr
 
+def solution(signals):
+    answer = -1
+    l = len(signals)
+    end = 2*10**6
+        
+    cnts = [0] * (end+1)
+    for i in range(l):
+        g, y, r = signals[i]
+        start = g + 1
+        while start <= end:
+            for j in range(start, min(start+y, end)):
+                cnts[j] += 1
+            start += g+y+r
+    
+    for tick in range(2, end):
+        if cnts[tick] == l:
+            answer = tick
+            break
+    
+    return answer
+
 signals = signals_input()
-l = len(signals)
-end = 10**6
-# for i in range(l):
-#     end = math.lcm(end, sum(signals[i]))
-
-
-cnts = [0] * (end+1)
-for i in range(l):
-    g, y, r = signals[i]
-    start = g + 1
-    while start <= end:
-        for j in range(start, min(start+y, end)):
-            cnts[j] += 1
-        start += g+y+r
-
-answer = -1
-for tick in range(2, end):
-    if cnts[tick] == l:
-        answer = tick
-        break
-print(answer)
+print(solution(signals))
