f3f1ace5a59b0c0cdd44d3574e79e09057b4934a
sangjun12458 <sjpark514@khu.ac.kr>
2026-05-17 23:55:18 +0900

    fix(programmers-택배 상자 꺼내기): 빼낼 상자 개수 계산 수정

--- problems/programmers/lv1/package/main.py
+++ problems/programmers/lv1/package/main.py
@@ -7,7 +7,6 @@ def solution(n, w, num):
     r = n % (2*w)
     sh2 = r // w
     r %= w
-
     height = 2 * sh1 + sh2
 
     # 원하는 택배의 높이
@@ -15,10 +14,15 @@ def solution(n, w, num):
     num_r = num % (2*w)
     num_sh2 = num_r // w
     num_r %= w
-
     num_height = 2 * num_sh1 + num_sh1 + (1 if num_r else 0)
-    num_idx = w - num_r if num_sh2 else num_r-1
 
-    answer = num_height - height
+    # 빼낼 상자 개수 계산
+    answer = height - num_height
+    if r ^ num_r:
+        answer += 1 if r + num_r > w else 0
+    else:
+        answer += 1 if r <= num_r else 0
 
     return answer
+
+solution(22, 6, 8)
