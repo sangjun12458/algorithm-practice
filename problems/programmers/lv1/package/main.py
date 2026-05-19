ac4a7589a7f92fd6e4ac35cad9149a4ca71f1a28
sangjun12458 <sjpark514@khu.ac.kr>
2026-05-19 11:31:07 +0900

    feat(progammers-택배 상자 꺼내기): complete solution with list

--- problems/programmers/lv1/package/main.py
+++ problems/programmers/lv1/package/main.py
@@ -1,28 +1,36 @@
 
 def solution(n, w, num):
     answer = 0
+    num -= 1
     
     # 전체 택배의 높이
     sh1 = n // (2*w)
     r = n % (2*w)
     sh2 = r // w
     r %= w
-    height = 2 * sh1 + sh2
+    heights = [2 * sh1 + sh2] * w
+    if sh2:
+        for i in range(-1, -1-r, -1):
+            heights[i] += 1
+    else:
+        for i in range(r):
+            heights[i] += 1
+    #height = 2 * sh1 + sh2
 
     # 원하는 택배의 높이
     num_sh1 = num // (2*w)
     num_r = num % (2*w)
     num_sh2 = num_r // w
     num_r %= w
-    num_height = 2 * num_sh1 + num_sh1 + (1 if num_r else 0)
+    num_height = 2 * num_sh1 + num_sh2
 
     # 빼낼 상자 개수 계산
-    answer = height - num_height
-    if r ^ num_r:
-        answer += 1 if r + num_r > w else 0
-    else:
-        answer += 1 if r <= num_r else 0
-
+    if num_sh2:
+        answer = heights[w-1-num_r] - num_height
+    else: 
+        answer = heights[num_r] - num_height
+    # if sh2 ^ num_sh2:
+    #     answer -= 1 if r + num_r <= w else 0
+    # else:
+    #     answer -= 1 if r < num_r else 0
     return answer
-
-solution(22, 6, 8)
