b7ef097c0264db721397544117009a85ffaf862c
sangjun12458 <sjpark514@khu.ac.kr>
2026-05-21 23:26:29 +0900

    feat(programmers-유연근무제): complete solution

--- problems/programmers/lv1/work_system/main.py
+++ problems/programmers/lv1/work_system/main.py
@@ -3,13 +3,15 @@ def solution(schedules, timelogs, startday):
 
     for idx, s in enumerate(schedules):
         target_time = s // 100 * 60 + s % 100
+        check_bonus = True
         for day in range(7):
             today = (startday + day) % 7
             if today in [6, 0]: continue
             log = timelogs[idx][day]
             today_time = log // 100 * 60 + log % 100
-            
-
-
+            if today_time - target_time > 10:
+                check_bonus = False
+        if check_bonus:
+            answer += 1
 
     return answer
