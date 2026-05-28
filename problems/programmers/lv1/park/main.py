a0252595d5973decbefd01c38d3299374cb16219
sangjun12458 <sjpark514@khu.ac.kr>
2026-05-28 21:35:07 +0900

    feat(programmers-공원): 깔 수 있는 돗자리 최대 크기 계산 코드 진행 중

--- problems/programmers/lv1/park/main.py
+++ problems/programmers/lv1/park/main.py
@@ -1,8 +1,23 @@
 def solution(mats, park):
     answer = 0
 
-    for r in range(len(park)):
-        for c in range(len(park[r])):
-            pass
+    row_len = len(park)
+    col_len = len(park[0])
+    max_len = max(mats)
 
+    for r in range(row_len):
+        for c in range(col_len):
+            check_square = [True] * max_len
+            if park[r][c] != '-1':
+                continue
+            for dr in range(max_len):
+                nr = r + dr
+                if nr >= row_len:
+                    continue
+                for dc in range(max_len):
+                    nc = c + dc
+                    if nc >= col_len:
+                        continue
+                    if park[nr][nc] != '-1':
+                        pass
     return answer
