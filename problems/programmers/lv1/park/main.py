ce93cd6680aa64227eb3ef879c6941e37e2a81e4
sangjun12458 <sjpark514@khu.ac.kr>
2026-06-03 23:48:49 +0900

    fix(programmers-공원): 돗자리 펼칠 수 있는 지 확하 하는 코 인하는 코드 수정

--- problems/programmers/lv1/park/main.py
+++ problems/programmers/lv1/park/main.py
@@ -5,23 +5,19 @@ def solution(mats, park):
     col_len = len(park[0])
     max_len = max(mats)
 
+    def check(r, c, l):
+        for i in range(r, r+l):
+            for j in range(c, c+l):
+                if park[i][j] != -1:
+                    return False
+        return True
+
     for r in range(row_len):
         for c in range(col_len):
-            check_square = [True] * max_len
-            if park[r][c] != '-1':
-                continue
-            for dr in range(max_len):
-                nr = r + dr
-                if nr >= row_len:
-                    continue
-                for dc in range(max_len):
-                    nc = c + dc
-                    if nc >= col_len:
-                        continue
-                    if park[nr][nc] != '-1':
-                        check_square[max(dr, dc)] = False
-            for idx, x in enumerate(check_square):
-                if x and idx in mats:
-                    answer = max(answer, idx)
+            for l in mats:
+                if check(r, c, l):
+                    answer = max(answer, l)
+                else:
+                    break
 
     return answer
