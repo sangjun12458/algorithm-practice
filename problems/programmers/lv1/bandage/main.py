bdcda9a67bb0f2ac3d739d9f47dea39056b417c5
sangjun12458 <sjpark514@khu.ac.kr>
2026-06-11 23:25:16 +0900

    feat(programmers-붕대감기): 풀이 흐름 작성

--- problems/programmers/lv1/bandage/main.py
+++ problems/programmers/lv1/bandage/main.py
@@ -1,7 +1,14 @@
 def solution(bandage, health, attacks):
     answer = 0
 
-    for attack in attacks:
+    for t, damage in attacks[:-1]:
+        # 회복중인 경우에 대한 처리
+
+        # 피해
+        health -= damage
+
+        # 다시 붕대 감기
+
         pass
 
     return answer
