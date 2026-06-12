3e810cf822271457d797a27f0cb86f1346a9fd0b
sangjun12458 <sjpark514@khu.ac.kr>
2026-06-12 23:52:18 +0900

    feat(programmers-붕대감기): 초당 회복 추가

--- problems/programmers/lv1/bandage/main.py
+++ problems/programmers/lv1/bandage/main.py
@@ -1,13 +1,16 @@
 def solution(bandage, health, attacks):
     answer = 0
 
-    for t, damage in attacks[:-1]:
+    for idx, t, damage in enumerate(attacks[:-1]):
         # 회복중인 경우에 대한 처리
 
         # 피해
         health -= damage
 
         # 다시 붕대 감기
+        nt = attacks[idx+1][0]
+        interval = nt - t
+        health += interval
 
         pass
 
