9566d8a0ed943b8cb5731f03a4cdf3cdfede7cfd
sangjun12458 <sjpark514@khu.ac.kr>
2026-06-14 23:55:23 +0900

    feat(programmer-붕대감기): 붕대 감는 시간과 공격 시간에

--- problems/programmers/lv1/bandage/main.py
+++ problems/programmers/lv1/bandage/main.py
@@ -2,18 +2,17 @@ def solution(bandage, health, attacks):
     answer = 0
 
     for idx, t, damage in enumerate(attacks[:-1]):
-        # 회복중인 경우에 대한 처리
-
         # 피해
         health -= damage
 
-        if health <= 0 return -1
+        if health <= 0:
+            return -1
 
         # 다시 붕대 감기
         nt = attacks[idx+1][0]
         interval = nt - t
-        health += interval
-
-        pass
+        health += interval * bandage[1]
+        if interval >= bandage[0]:
+            health += bandage[2]
 
     return answer
