e2824786943bed2ef1c3269d96732cd6a052052c
sangjun12458 <sjpark514@khu.ac.kr>
2026-06-15 23:09:00 +0900

    fix(programmers-붕대감기): 추가 회복 처리 중

--- problems/programmers/lv1/bandage/main.py
+++ problems/programmers/lv1/bandage/main.py
@@ -1,12 +1,10 @@
 def solution(bandage, health, attacks):
-    answer = 0
+    answer = health
+    # last_attack_time = attacks[-1][0]
+    # for t in range(1, last_attack_time+1):
+    #     pass
 
-    for idx, t, damage in enumerate(attacks[:-1]):
-        # 피해
-        health -= damage
-
-        if health <= 0:
-            return -1
+    for idx, (t, damage) in enumerate(attacks):
 
         # 다시 붕대 감기
         nt = attacks[idx+1][0]
@@ -14,6 +12,10 @@ def solution(bandage, health, attacks):
         health += interval * bandage[1]
         if interval >= bandage[0]:
             health += bandage[2]
-            
 
-    return answer
+        # 피해
+        health -= damage
+        if health <= 0:
+            return -1
+
+    return health
