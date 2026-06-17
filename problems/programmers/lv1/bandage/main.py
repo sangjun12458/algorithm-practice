0e07b307073de9d8db49b646efa42e9ef33c664b
sangjun12458 <sjpark514@khu.ac.kr>
2026-06-17 23:51:38 +0900

    fix(programmers-붕대감기): complete solution with error correction

--- problems/programmers/lv1/bandage/main.py
+++ problems/programmers/lv1/bandage/main.py
@@ -1,14 +1,12 @@
 def solution(bandage, health, attacks):
     max_health = health
-    # last_attack_time = attacks[-1][0]
-    # for t in range(1, last_attack_time+1):
-    #     pass
     pt = 0
+    
     for idx, (t, damage) in enumerate(attacks):
         # 회복
-        interval = t - pt
+        interval = t - pt - 1
         health += interval * bandage[1]
-        health += interval % bandage[0] * bandage[2]
+        health += interval // bandage[0] * bandage[2]
         health = min(health, max_health)
 
         # 피해
@@ -16,6 +14,7 @@ def solution(bandage, health, attacks):
         if health <= 0:
             break
 
+        print(interval, bandage[1], health)
         pt = attacks[idx][0]
 
     return health if health > 0 else -1
