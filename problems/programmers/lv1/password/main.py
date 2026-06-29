c5130dda3407d2f244e52e55ed1ec96b8eab2d56
sangjun12458 <sjpark514@khu.ac.kr>
2026-06-29 21:33:55 +0900

    fix(prgrms-password): 알파벳 별 변환 필요)

--- problems/programmers/lv1/password/main.py
+++ problems/programmers/lv1/password/main.py
@@ -1,9 +1,13 @@
 def solution(s, skip, index):
     answer = ''
 
+    shifted = [0] * 26
+
+    for i in range(26):
+        pass
+        # 각 알파벳 별 계산
+
     for x in s:
-        if s not in skip:
-            x = (x - 'a' + index) % 26 + 'a'
-        answer += x
+        answer += shifted[ord(x) - ord('a')]
 
     return answer
