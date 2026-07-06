a72b01ab0383f036869fa629170a34e64654cb73
sangjun12458 <sjpark514@khu.ac.kr>
2026-07-06 23:45:42 +0900

    feat(prgrms-paint_over): 함수 기본 틀 작성

--- problems/programmers/lv1/paint_over/main.py
+++ problems/programmers/lv1/paint_over/main.py
@@ -1,3 +1,14 @@
 def solution(n, m, section):
     answer = 0
+
+    painted = [0] * (n+1)
+    for s in section:
+        painted[s] = -1
+
+    # preprocess
+
+    while True:
+        # paint
+        break
+
     return answer
