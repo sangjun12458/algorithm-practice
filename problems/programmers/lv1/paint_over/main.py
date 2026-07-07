183be0154f267074ebe11ef9290bdc21c4478e98
sangjun12458 <sjpark514@khu.ac.kr>
2026-07-07 23:42:31 +0900

    feat(prgrms-paint_over): Add complete solution (sequential processing)

--- problems/programmers/lv1/paint_over/main.py
+++ problems/programmers/lv1/paint_over/main.py
@@ -1,14 +1,20 @@
 def solution(n, m, section):
     answer = 0
 
-    painted = [0] * (n+1)
-    for s in section:
-        painted[s] = -1
+    start = section[0]
+    answer += 1
+    for s in section[1:]:
+        s_num = int(s)
+        end = start + m - 1
+        if s_num <= end:
+            continue
+        else:
+            start = s_num
+            answer += 1
 
     # preprocess
-
-    while True:
-        # paint
-        break
+    # while True:
+    #     # paint
+    #     break
 
     return answer
