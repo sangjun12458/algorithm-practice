ee20fdb151809228f8f85a673fe02c323d235b73
sangjun12458 <sjpark514@khu.ac.kr>
2026-07-11 23:52:45 +0900

    feat(prgrms-wallpaper): 입력값을 좌표로 변환

--- problems/programmers/lv1/wallpaper/main.py
+++ problems/programmers/lv1/wallpaper/main.py
@@ -1,3 +1,10 @@
 def solution(wallpaper):
     answer = []
+
+    coords = []
+    for idx_r, row in enumerate(wallpaper):
+        for idx_c, c in enumerate(row):
+            if c == '#':
+                coords.append((idx_r, idx_c))
+
     return answer
