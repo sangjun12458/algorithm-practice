38bb49f44515d9d15a36f19bf63ead31c4276c1d
sangjun12458 <sjpark514@khu.ac.kr>
2026-07-14 20:48:24 +0900

    feat(prgrms-wallpaper): Add complete solution

--- problems/programmers/lv1/wallpaper/main.py
+++ problems/programmers/lv1/wallpaper/main.py
@@ -1,5 +1,6 @@
 def solution(wallpaper):
     answer = []
+    MAX_NUM = 50
 
     coords = []
     for idx_r, row in enumerate(wallpaper):
@@ -7,6 +8,13 @@ def solution(wallpaper):
             if c == '#':
                 coords.append((idx_r, idx_c))
 
-    minr, minc, maxr, maxc = 0, 0, 0, 0
-    
+    minr, minc, maxr, maxc = MAX_NUM, MAX_NUM, 0, 0
+    for y, x in coords:
+        minr = min(minr, y)
+        minc = min(minc, x)
+        maxr = max(maxr, y)
+        maxc = max(maxc, x)
+
+    answer.extend([minr, minc, maxr+1, maxc+1])
+
     return answer
