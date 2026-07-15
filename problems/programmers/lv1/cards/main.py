bdfcf439c5afd4ecda6df64bb8aae02a7f60d3fd
sangjun12458 <sjpark514@khu.ac.kr>
2026-07-15 23:58:06 +0900

    feat(prgrms-cards): select cards from each card pack and compare them using ptr

--- problems/programmers/lv1/cards/main.py
+++ problems/programmers/lv1/cards/main.py
@@ -1,11 +1,20 @@
 def solution(cards1, cards2, goal):
     answer = ''
 
-    checked = [(0, 0)] * len(goal)
-    p = 0
+    p1, p2, p3 = 0, 0, 0
+    while p3 < len(goal):
+        if p1 < len(cards1) and goal[p3] == cards1[p1]:
+            p1 += 1
+            p3 += 1
+        elif p2 < len(cards2) and goal[p3] == cards2[p2]:
+            p2 += 1
+            p3 += 1
+        else:
+            break
 
-    for g in goal:
-        print(cards1.index(g))
-        
+    if p1 == len(cards1)-1 and p2 == len(cards2)-1 and p3 == len(cards3)-1:
+        answer = "Yes"
+    else:
+        answer = 'No'
 
     return answer
