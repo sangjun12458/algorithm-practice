04cff906db84647aabb1770c368319144c123729
sangjun12458 <sjpark514@khu.ac.kr>
2026-07-18 22:17:33 +0900

    fix(prgrms-cards): 남은 테스트 케이스 통과를 위해 코드 개선 중 하지만 아직 해결하지 못함

--- problems/programmers/lv1/cards/main.py
+++ problems/programmers/lv1/cards/main.py
@@ -1,17 +1,18 @@
 def solution(cards1, cards2, goal):
     answer = ''
-
+    l1, l2, l3 = len(cards1), len(cards2), len(goal)
     p1, p2, p3 = 0, 0, 0
-    while p3 < len(goal):
-        if p1 < len(cards1) and goal[p3] == cards1[p1]:
+    while p3 < l3 and (p1 < l1 or p2 < l2):
+        if p1 < l1 and goal[p3] == cards1[p1]:
             p1 += 1
             p3 += 1
-        elif p2 < len(cards2) and goal[p3] == cards2[p2]:
+        elif p2 < l2 and goal[p3] == cards2[p2]:
             p2 += 1
             p3 += 1
         else:
-            break
-
+            p1 += 1
+            p2 += 1
+            
     if p1 == len(cards1) and p2 == len(cards2) and p3 == len(goal):
         answer = "Yes"
     else:
