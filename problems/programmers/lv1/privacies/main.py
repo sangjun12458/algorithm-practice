03b0a17605ec8fa0c9d8861eb06227c11716aeb1
sangjun12458 <sjpark514@khu.ac.kr>
2026-07-20 23:20:04 +0900

    feat(prgrms-privacies): 날짜를 숫자 값으로 변환

--- problems/programmers/lv1/privacies/main.py
+++ problems/programmers/lv1/privacies/main.py
@@ -1,3 +1,9 @@
 def solution(today, terms, privacies):
     answer = []
+
+    y, m, d = map(int, today.split('.'))
+
+    for privacy in privacies:
+        py, pm, pd = map(int, privacy.split('.'))
+
     return answer
