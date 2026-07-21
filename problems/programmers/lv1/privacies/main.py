13b39ad93e15b343fa022d14087827c72d4ef297
sangjun12458 <sjpark514@khu.ac.kr>
2026-07-21 23:45:50 +0900

    feat(prgrms-privacies): Terms and condition expriation period save in dcit type

--- problems/programmers/lv1/privacies/main.py
+++ problems/programmers/lv1/privacies/main.py
@@ -2,6 +2,11 @@ def solution(today, terms, privacies):
     answer = []
 
     y, m, d = map(int, today.split('.'))
+    
+    e_period = dict()
+    for term in terms:
+        t, p = term.split(' ')
+        e_period[t] = int(p)
 
     for privacy in privacies:
         py, pm, pd = map(int, privacy.split('.'))
