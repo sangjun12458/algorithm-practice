c0c8571536721fa42ef9d0761b2481fbd032561e
sangjun12458 <sjpark514@khu.ac.kr>
2026-07-23 23:32:17 +0900

    feat(prgrms-privacies): incomplete solution - some test cases failed

--- problems/programmers/lv1/privacies/main.py
+++ problems/programmers/lv1/privacies/main.py
@@ -8,12 +8,17 @@ def solution(today, terms, privacies):
         t, p = term.split(' ')
         e_period[t] = int(p)
 
-    for privacy in privacies:
+    for idx, privacy in enumerate(privacies):
         pp, pt = privacy.split(' ')
         py, pm, pd = map(int, pp.split('.'))
         ep = e_period[pt]
         py = py + (pm + ep) // 12
         pm = (pm + ep) % 12
         
+        today = y * 10000 + m * 100 + d
+        deadline = py * 10000 + pm * 100 + pd
+        print(today, deadline)
+        if deadline <= today:
+            answer.append(idx+1)
 
     return answer
