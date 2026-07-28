445810fc9669dc020d4ba21f30e3056b0560e668
sangjun12458 <sjpark514@khu.ac.kr>
2026-07-28 23:55:05 +0900

    feat(prgrms-keyboard): Store the number of key presses required to input each letter of the alphabet.

--- problems/programmers/lv1/keyboard/main.py
+++ problems/programmers/lv1/keyboard/main.py
@@ -1,3 +1,18 @@
 def solution(keymap, targets):
     answer = []
+
+    press_numbers = dict()
+    for x in range(26):
+        press_numbers[chr(ord('a') + x)] = 101
+    for key in keymap:
+        for idx, alphabet in enumerate(key):
+            press_numbers[alphabet] = min(idx, press_numbers[alphabet])
+
+    def typing():
+        result = -1
+        return result
+
+    for target in targets:
+        answer.append(typing(target))
+
     return answer
