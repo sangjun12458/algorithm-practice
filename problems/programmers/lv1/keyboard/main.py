99e1bf27ef76e0183ab416bed21187734519fa9d
sangjun12458 <sjpark514@khu.ac.kr>
2026-07-30 23:59:43 +0900

    feat(prgrms-keyboard): handle cases where input is impossible

--- problems/programmers/lv1/keyboard/main.py
+++ problems/programmers/lv1/keyboard/main.py
@@ -1,17 +1,24 @@
 def solution(keymap, targets):
     answer = []
-
+    keymap = ["ABACD", "BCEFD"]
+    targets = ["ABCD", "AABB"]
     press_numbers = dict()
     for x in range(26):
-        press_numbers[chr(ord('a') + x)] = 101
+        press_numbers[chr(ord('A') + x)] = 101
     for key in keymap:
         for idx, alphabet in enumerate(key):
             press_numbers[alphabet] = min(idx, press_numbers[alphabet])
-
+    print(press_numbers)
     for target in targets:
         cnt = 0
         for x in target:
+            t = press_numbers[x]
+            if t == 101:
+                cnt = -1
+                break
             cnt += press_numbers[x]
         answer.append(cnt)
 
     return answer
+
+print(solution(None, None))
