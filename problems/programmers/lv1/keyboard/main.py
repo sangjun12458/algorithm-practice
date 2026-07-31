28c3cbc6c3de2e9374bf209ba75a89dfdc5e9756
sangjun12458 <sjpark514@khu.ac.kr>
2026-07-31 23:51:00 +0900

    feat(prgrms-keyboard): Add a complete solution

--- problems/programmers/lv1/keyboard/main.py
+++ problems/programmers/lv1/keyboard/main.py
@@ -1,14 +1,13 @@
 def solution(keymap, targets):
     answer = []
-    keymap = ["ABACD", "BCEFD"]
-    targets = ["ABCD", "AABB"]
+
     press_numbers = dict()
     for x in range(26):
         press_numbers[chr(ord('A') + x)] = 101
     for key in keymap:
         for idx, alphabet in enumerate(key):
-            press_numbers[alphabet] = min(idx, press_numbers[alphabet])
-    print(press_numbers)
+            press_numbers[alphabet] = min(idx + 1, press_numbers[alphabet])
+
     for target in targets:
         cnt = 0
         for x in target:
@@ -20,5 +19,3 @@ def solution(keymap, targets):
         answer.append(cnt)
 
     return answer
-
-print(solution(None, None))
