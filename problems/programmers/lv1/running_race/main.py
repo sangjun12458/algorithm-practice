f71e54a497c08254b91441843bf5e3663b194788
sangjun12458 <sjpark514@khu.ac.kr>
2026-08-01 23:37:51 +0900

    feat(prgrms-running_race): Add incomplete solution with TLE

---
+++ problems/programmers/lv1/running_race/main.py
@@ -0,0 +1,9 @@
+def solution(players : list, callings):
+    from copy import deepcopy
+    answer = deepcopy(players)
+
+    for c in callings:
+        i = answer.index(c)
+        answer[i-1], answer[i] = answer[i], answer[i-1]
+
+    return answer
