af0ac90c67c6c583edf8d7e6f663b4e87f86dcce
sangjun12458 <sjpark514@khu.ac.kr>
2026-08-05 23:51:41 +0900

    feat(prgrms-running_race): 순위 기록 리스트 추가

--- problems/programmers/lv1/running_race/main.py
+++ problems/programmers/lv1/running_race/main.py
@@ -2,6 +2,8 @@ def solution(players : list, callings):
     from copy import deepcopy
     answer = deepcopy(players)
 
+    ranks = [0] * len(players)
+
     for c in callings:
         i = answer.index(c)
         answer[i-1], answer[i] = answer[i], answer[i-1]
