498d0f5aa87a448c522bc362e565fed362c5fad5
sangjun12458 <sjpark514@khu.ac.kr>
2026-08-06 22:32:37 +0900

    feat(prgrms-running_race): Add complete solution with dict

--- problems/programmers/lv1/running_race/main.py
+++ problems/programmers/lv1/running_race/main.py
@@ -1,11 +1,12 @@
 def solution(players : list, callings):
     from copy import deepcopy
     answer = deepcopy(players)
+    ranks = dict([(name, idx) for idx, name in enumerate(answer)])
 
-    ranks = [0] * len(players)
-
-    for c in callings:
-        i = answer.index(c)
-        answer[i-1], answer[i] = answer[i], answer[i-1]
+    for name in callings:
+        r = ranks[name]
+        ranks[name] = r-1
+        ranks[answer[r-1]] = r
+        answer[r-1], answer[r] = answer[r], answer[r-1]
 
     return answer
