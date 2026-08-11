f9a84b195798e2858ad7ee481bb5659a50a99891
sangjun12458 <sjpark514@khu.ac.kr>
2026-08-12 00:13:40 +0900

    feat(prgrms-split_string): Add complete solution

--- problems/programmers/lv1/split_string/main.py
+++ problems/programmers/lv1/split_string/main.py
@@ -1,9 +1,23 @@
 def solution(s):
     answer = 0
 
-    start = 0
-    end = 0
-    while True:
-        break
+    x = ''
+    cnt_x = 0
+    cnt_not_x = 0
+    for c in s:
+        if x == '':
+            x = c
+            cnt_x += 1
+        elif x == c:
+            cnt_x += 1
+        else:
+            cnt_not_x += 1
+        if cnt_x == cnt_not_x:
+            x = ''
+            cnt_x = 0
+            cnt_not_x = 0
+            answer += 1
+    if x != '':
+        answer += 1
 
     return answer
