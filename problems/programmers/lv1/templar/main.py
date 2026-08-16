47d6dd741331547dbf38b38ad9677c7fa40b8120
sangjun12458 <sjpark514@khu.ac.kr>
2026-08-16 23:43:47 +0900

    feat(prgrms-templar): Add complete solution

--- problems/programmers/lv1/templar/main.py
+++ problems/programmers/lv1/templar/main.py
@@ -1,18 +1,15 @@
 def solution(number, limit, power): 
     answer = 0
 
-    for x in range(number):
+    for x in range(1, number+1):
         cnt = 0
         y = 1
-        while y * y <= x:
+        while y * y < x:
             if x % y == 0:
                 cnt += 2
             y += 1
-        print(x, cnt)
+        if y * y == x:
+            cnt += 1
         answer += cnt if cnt <= limit else power
 
     return answer
-
-
-number, limit, power = 	5, 3, 2
-solution(number, limit, power)
