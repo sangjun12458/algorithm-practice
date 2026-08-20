8aee8ce47aae0066bf47a12501656c22ec533c67
sangjun12458 <sjpark514@khu.ac.kr>
2026-08-20 20:48:01 +0900

    Feat(prgrms-spoiler): Add complete solution - 단어 별 스포일러 방지 처리 여부 구분. 이후, 단어 별 개수 카운트하여 중요한 단어 판별

--- problems/programmers/lv1/spoiler/main.py
+++ problems/programmers/lv1/spoiler/main.py
@@ -1,19 +1,36 @@
-def solution(message, spoiler_ranges):
+def solution(message : str, spoiler_ranges : list):
     answer = 0
+
+    words = message.split(' ')
     idx_to_word_list = [0] * len(message)
-    word_dict = dict()
     word_num = 0
     for idx, c in enumerate(message):
-        idx_to_word_list[idx] = word_num
         if c == ' ':
+            idx_to_word_list[idx] = -1
             word_num += 1
-    
+        else:
+            idx_to_word_list[idx] = word_num
+
+    processed = [False] * len(words)
     for start, end in spoiler_ranges:
-        print(message[start:end+1])
+        for i in range(start, end + 1):
+            word_num = idx_to_word_list[i]
+            if word_num == -1:
+                continue
+            processed[word_num] = True
+
+    word_dict = dict()
+    for i, is_processed in enumerate(processed):
+        if not is_processed: continue
+        word = words[i]
+        if word_dict.get(word):
+            word_dict[word] += 1
+        else:
+            word_dict[word] = 1
+
+    for key, value in word_dict.items():
+        cnt = words.count(key)
+        if cnt == value:
+            answer += 1
 
     return answer
-
-message, spoiler_ranges = "my phone number is 01012345678 and may i have your phone number", [[5, 5], [25, 28], [34, 40], [53, 59]]
-k = message.index()
-print(k)
-solution(message, spoiler_ranges)
