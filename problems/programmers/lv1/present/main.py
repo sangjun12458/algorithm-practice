3cb3dae13cb769732ba7e194ba805b79342ef1bb
sangjun12458 <sjpark514@khu.ac.kr>
2026-08-28 23:10:31 +0900

    feat(prgrms-present): Add complete solution - 다음달 받을 선물 중복 집계 제거

--- problems/programmers/lv1/present/main.py
+++ problems/programmers/lv1/present/main.py
@@ -12,35 +12,26 @@ def solution(friends, gifts):
         r_num = nums[receiver]
         board[s_num][r_num] += 1
         board[r_num][s_num] -= 1
-    
-    #best_sender = -1
-    # best_score = -100
-    # for i in range(n):
-    #     s_total = sum(board[i])
-    #     r_total = sum([board[j][i] for j in range(n)])
-    #     score = s_total - r_total
-    #     if score > best_score:
-    #         #best_sender = i
-    #         best_score = score
-    # answer = best_score
+
+    total = [0] * n
 
     for i in range(n):
         for j in range(i+1, n):
             score = board[i][j]
             if score > 0:
-                board[i][i] += 1
+                total[i] += 1
             elif score < 0:
-                board[j][j] += 1
+                total[j] += 1
             else:
                 i_total = sum(board[i])
                 j_total = sum(board[j])
                 if i_total > j_total:
-                    board[i][i] += 1
+                    total[i] += 1
                 elif i_total < j_total:
-                    board[j][j] += 1
+                    total[j] += 1
 
-    answer = -100
+    answer = -10000
     for i in range(n):
-        answer = max(answer, board[i][i])
+        answer = max(answer, total[i])
 
     return answer
