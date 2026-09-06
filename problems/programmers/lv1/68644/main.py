2fff02136c3d8266c53a733491d575e722893ed4
sangjun12458 <sjpark514@khu.ac.kr>
2026-09-06 22:33:00 +0900

    feat(prgrms-68644): Add complete solution with using 'set'

--- problems/programmers/lv1/68644/main.py
+++ problems/programmers/lv1/68644/main.py
@@ -1,3 +1,14 @@
 def solution(numbers):
     answer = []
+
+    l = len(numbers)
+    s = set()
+    for i in range(l):
+        x = numbers[i]
+        for j in range(i+1, l):
+            s.add(x + numbers[j])
+
+    answer.extend(s)
+    answer.sort()
+
     return answer
