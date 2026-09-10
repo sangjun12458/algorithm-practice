917a37f574d38793471db9392ee0491ed831cf91
sangjun12458 <sjpark514@khu.ac.kr>
2026-09-10 23:59:21 +0900

    feat(prgrms-67256): processs each number in numbers with case based on keypad

--- problems/programmers/courses30/67256/main.py
+++ problems/programmers/courses30/67256/main.py
@@ -1,3 +1,15 @@
 def solution(numbers, hand):
     answer = ''
+
+    lh = -1
+    rh = -1
+
+    for n in numbers:
+        if n in [1, 4, 7]:
+            pass
+        elif n in [3, 6, 9]:
+            pass
+        else:
+            pass
+
     return answer
