f15866d246c3cbdcb13f19f13466359b9eb37970
sangjun12458 <sjpark514@khu.ac.kr>
2026-09-15 23:55:11 +0900

    feat(prgrms-71410): 마침표 처리 과정 추가

--- problems/programmers/courses30/72410/main.py
+++ problems/programmers/courses30/72410/main.py
@@ -5,13 +5,15 @@ def solution(new_id : str):
 
     # 1. uppercase to lowercase
     new_id = new_id.lower()
-    # 2. proper character refine
+    # 2. proper character refine, 3. unique full stop
     temp = ''
+    prev = ''
     for c in new_id:
         if 'a' <= c <= 'z' or 0 <= c <= 9 or c in ['-', '_', '.']:
             temp.append(c)
-    # 3. unique full stop
-    
+        if prev == '.' and c == '.':
+            continue
+
     # 4. 
     # 5.
     # 6.
