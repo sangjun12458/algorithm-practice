470719348f88725f0f8ea67cfd2f7448902d1586
sangjun12458 <sjpark514@khu.ac.kr>
2026-09-18 23:54:31 +0900

    feat(prgrms-64061): 문제 풀이 시작

---
+++ problems/programmers/courses30/64061/main.py
@@ -0,0 +1,13 @@
+from collections import stack
+
+def solution(board, moves):
+    answer = 0
+
+    ss = []
+    for b in board:
+        s = stack()
+        for x in b:
+            pass
+
+
+    return answer
