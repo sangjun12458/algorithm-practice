1d5316a80dfde39c6cc5cd7a4bbfeba990e389ac
sangjun12458 <sjpark514@khu.ac.kr>
2026-09-19 02:09:50 +0900

    feat(prgrms-64061): 인형뽑기 절차 추가 - stack 사용

--- problems/programmers/courses30/64061/main.py
+++ problems/programmers/courses30/64061/main.py
@@ -1,13 +1,31 @@
-from collections import stack
+# 크레인 인형뽑기 게임
 
 def solution(board, moves):
     answer = 0
 
-    ss = []
+    ss = [[]]
     for b in board:
-        s = stack()
+        s = []
         for x in b:
-            pass
+            if x:
+                s.append(x)            
+        ss.append(s)
 
+    basket = []
+    for m in moves:
+        if not ss[m]:
+            continue
+        x = ss[m][-1]
+        if basket and basket[-1] == x:
+            basket.pop()
+            answer += 1
+        else:
+            basket.append(x)
 
     return answer
+
+board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]	
+moves = [1,5,3,5,1,2,1,4]
+
+answer = solution(board, moves)
+print(answer)
