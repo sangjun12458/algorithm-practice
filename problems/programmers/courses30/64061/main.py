57c9d659548389ac6eb73f4f451ac67ee953a891
sangjun12458 <sjpark514@khu.ac.kr>
2026-09-19 15:58:51 +0900

    Feat(prgrms-64061): Solved the example case, but failed other test cases

--- problems/programmers/courses30/64061/main.py
+++ problems/programmers/courses30/64061/main.py
@@ -15,17 +15,11 @@ def solution(board, moves):
     for m in moves:
         if not ss[m]:
             continue
-        x = ss[m][-1]
+        x = ss[m].pop()
         if basket and basket[-1] == x:
             basket.pop()
-            answer += 1
+            answer += 2
         else:
             basket.append(x)
 
     return answer
-
-board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]	
-moves = [1,5,3,5,1,2,1,4]
-
-answer = solution(board, moves)
-print(answer)
