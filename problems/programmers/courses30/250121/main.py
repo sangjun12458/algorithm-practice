bd7d9a90b5c5e58c3939e7145ebf5eb5cba81cf2
sangjun12458 <sjpark514@khu.ac.kr>
2026-09-23 23:10:09 +0900

    feat(prgrms-250121): 문제풀이 시작

---
+++ problems/programmers/courses30/250121/main.py
@@ -0,0 +1,17 @@
+# [PCCE 기출문제] 10번 / 데이터 분석
+
+def solution(data, ext, val_ext, sort_by):
+    answer = [[]]
+
+    # criteria_num = 0
+    # if ext == 'date':
+    #     criteria_num = 1
+    # elif ext == 'maximum':
+    #     criteria_num = 2
+    # elif ext == 'remain':
+    #     criteria_num = 3
+    for code, date, maximum, remain in data:
+        if ext == 'code':
+            pass
+
+    return answer
