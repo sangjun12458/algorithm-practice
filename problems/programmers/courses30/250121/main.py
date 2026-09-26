8269360da708e30ea367236b5ed3adcb693bc772
sangjun12458 <sjpark514@khu.ac.kr>
2026-09-26 23:08:28 +0900

    Feat(prgrms-250121): 기준에 적합한 데이터 선별 및 정렬

--- problems/programmers/courses30/250121/main.py
+++ problems/programmers/courses30/250121/main.py
@@ -1,9 +1,9 @@
 # [PCCE 기출문제] 10번 / 데이터 분석
 
 def solution(data, ext, val_ext, sort_by):
-    answer = [[]]
+    answer = []
 
-    for idx, code, date, maximum, remain in enumerate(data):
+    for idx, (code, date, maximum, remain) in enumerate(data):
         criteria_num = 0
         if ext == 'code':
             criteria_num = 0
@@ -13,9 +13,9 @@ def solution(data, ext, val_ext, sort_by):
             criteria_num = 2
         elif ext == 'remain':
             criteria_num = 3
-        if data[idx] < val_ext:
+        if data[idx][criteria_num] < val_ext:
             answer.append([code, date, maximum, remain])
 
-    answer.sort()
+    answer.sort(key=lambda x: x[criteria_num], reverse=True)
 
     return answer
